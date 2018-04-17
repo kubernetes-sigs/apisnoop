85e4da437cd5e81abd2abb98ec6f5275c87292e8
Chris McClimans <chris@hippiehacker.org>
2018-04-17 22:49:18 +1200

    removing audit-policy-file until data pvc mounts

--- dev/minikube/start-minikube-audit.sh
+++ dev/minikube/start-minikube-audit.sh
@@ -3,7 +3,6 @@ set -e
 set -x
 minikube start --v=4  --mount --mount-string=$PWD:/tmp/files \
          --feature-gates=AdvancedAuditing=true \
-         --extra-config=apiserver.audit-policy-file==/tmp/files/audit-policy.yaml \
          --extra-config=apiserver.audit-log-path=/tmp/files/audit.log
          #2>&1 | tee /tmp/minikube.log
 #sleep 15
