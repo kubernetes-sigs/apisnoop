148299dc96326ddc67d04a890c5832b5d4adc2b8
Chris McClimans <chris@hippiehacker.org>
2018-04-17 22:45:22 +1200

    Tracking down why apiserver isn't restarting.

--- dev/minikube/start-minikube-audit.sh
+++ dev/minikube/start-minikube-audit.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+set -e
+set -x
 minikube start --v=4  --mount --mount-string=$PWD:/tmp/files \
          --feature-gates=AdvancedAuditing=true \
+         --extra-config=apiserver.audit-policy-file==/tmp/files/audit-policy.yaml \
          --extra-config=apiserver.audit-log-path=/tmp/files/audit.log
          #2>&1 | tee /tmp/minikube.log
-
-ssh -i $(minikube ssh-key) docker@$(minikube ip) sudo /tmp/files/apiserver-config.patch.sh
+#sleep 15
+echo "Running this should update kube-apiserver.yaml and trigger a restart:"
+echo ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+    -i $(minikube ssh-key) docker@$(minikube ip) \
+    sudo /tmp/files/apiserver-config.patch.sh
