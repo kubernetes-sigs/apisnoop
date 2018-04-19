b38e5ada7609dfa32b6d686bce4eb3783ca69180
Rohan Fletcher <rohan@ii.coop>
2018-04-19 18:10:31 +1200

    Changed patching approach

--- dev/minikube/start-minikube-audit.sh
+++ dev/minikube/start-minikube-audit.sh
@@ -2,11 +2,12 @@
 set -e
 set -x
 minikube start --v=4  --mount --mount-string=$PWD:/tmp/files \
-         --feature-gates=AdvancedAuditing=true \
-         --extra-config=apiserver.audit-log-path=/tmp/files/audit.log
-         #2>&1 | tee /tmp/minikube.log
-#sleep 15
+         --feature-gates=AdvancedAuditing=true
+echo
 echo "Running this should update kube-apiserver.yaml and trigger a restart:"
 echo ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
     -i $(minikube ssh-key) docker@$(minikube ip) \
-    sudo /tmp/files/apiserver-config.patch.sh
+    sudo /tmp/files/apiserver-config-v2.patch.sh
+echo
+echo "Running this sets up a test server to receive events from the webhook:"
+echo "go run test-server.go"
