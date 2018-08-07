65ced7eacd4f404dad2fa684c8a432485b2eaab0
Katharine Berry <ktbry@google.com>
2018-08-07 13:27:33 -0700

    Add UA filtering UI to the frontend.

--- dev/e2e-coverage-view/www/conformance/conformance.js
+++ dev/e2e-coverage-view/www/conformance/conformance.js
@@ -1,5 +1,5 @@
 google.charts.load('current', {'packages': ['table']});
-google.charts.setOnLoadCallback(drawTable);
+google.charts.setOnLoadCallback(doSetup);
 
 let _cache = null;
 async function fetchData() {
@@ -8,11 +8,52 @@ async function fetchData() {
   }
   let req = await fetch('/api/v1/stats/endpoint_hits', {credentials: 'include'});
   let result = await req.json();
+  updateUAList(result);
   _cache = result;
   return result;
 }
 
-async function drawTable() {
+function updateUAList(results) {
+  let uas = new Set();
+  for (let entry of results) {
+    for (let hit of entry.hits) {
+      uas.add(hit.ua);
+    }
+  }
+  let sorted = Array.from(uas).sort();
+  for (let ua of sorted) {
+    let option = document.createElement('option');
+    option.setAttribute('value', ua);
+    option.innerText = ua;
+    document.getElementById('ua-filter').appendChild(option);
+  }
+}
+
+let _table = null;
+function getTable() {
+  if (!_table) {
+    _table = new google.visualization.Table(document.getElementById('table'));
+  }
+  return _table;
+}
+
+let _data = null;
+function getData() {
+  if (!_data) {
+    _data = new google.visualization.DataTable({
+      cols: [{id: 'name', label: 'Category', type: 'string'},
+        {id: 'coverage', label: 'API Coverage', type: 'number'},
+        {id: 'tests', label: 'Hits per covered API', type: 'number'}],
+      rows: []
+    });
+  }
+  _data.removeRows(0, _data.getNumberOfRows());
+  return _data;
+}
+
+async function drawTable(filter) {
+  filter = filter || '';
+  console.log(`filter: ${filter}`);
   let result = await fetchData();
   let categories = {};
   for (let entry of result) {
@@ -22,28 +63,26 @@ async function drawTable() {
     }
     categories[category].total++;
     if (hits.length > 0) {
-      categories[category].tested++;
-      categories[category].hits += hits.reduce((total, hit) => total + hit.count, 0);
+      let count = hits.reduce((total, hit) => total + ((filter === '' || hit.ua.indexOf(filter) === 0) ? hit.count : 0), 0);
+      if (count > 0) {
+        categories[category].tested++;
+        categories[category].hits += count;
+      }
     }
   }
 
   let rows = [];
   for (let category of Object.values(categories)) {
     let coverage = (category.tested / category.total);
-    rows.push({c: [
+    rows.push([
         {v: category.name},
         {v: coverage * 1000, f: `${category.tested} / ${category.total} <strong>(${Math.round(coverage * 100)}%)</strong>`},
-        {v: Math.round(category.hits / category.tested), f: Math.round(category.hits / category.tested) || ''}
-    ]})
+        {v: Math.round(category.hits / category.tested), f: (Math.round(category.hits / category.tested) || '').toString()}
+    ])
   }
 
-
-  let data = new google.visualization.DataTable({
-    cols: [{id: 'name', label: 'Category', type: 'string'},
-      {id: 'coverage', label: 'API Coverage', type: 'number'},
-      {id: 'tests', label: 'Hits per covered API', type: 'number'}],
-    rows: rows
-  });
+  let data = getData();
+  data.addRows(rows);
 
   let colourFormatter = new google.visualization.ColorFormat();
   colourFormatter.addGradientRange(0, 1001, '#FFFFFF', '#DD0000', '#00DD00');
@@ -52,19 +91,29 @@ async function drawTable() {
   let barFormatter = new google.visualization.BarFormat({width: 75});
   barFormatter.format(data, 2);
 
-  let table = new google.visualization.Table(document.getElementById('table'));
-  table.draw(data, {allowHtml: true});
-  google.visualization.events.addListener(table, 'select', () => rowSelected(result, rows[table.getSelection()[0].row].c[0].v));
+  let sort = getTable().getSortInfo();
+  let selection = getTable().getSelection();
+
+  let sortDict = sort ? {sortAscending: sort.ascending, sortColumn: sort.column} : {};
+
+  getTable().draw(data, {allowHtml: true, ...sortDict});
+  getTable().setSelection(selection);
+  google.visualization.events.removeAllListeners(getTable());
+  google.visualization.events.addListener(getTable(), 'select', () => rowSelected(filter, result, rows[getTable().getSelection()[0].row][0].v));
+  if (selection.length) {
+    rowSelected(filter, result, rows[selection[0].row][0].v);
+  }
 }
 
-function rowSelected(result, category) {
+let _detailTable = null;
+function rowSelected(filter, result, category) {
   let information = result.filter(({category: c}) => c === category);
   let rows = [];
   for (let {method, url, hits} of information) {
     rows.push({c: [
         {v: url},
         {v: method},
-        {v: hits.reduce((total, hit) => total + hit.count, 0)}
+        {v: hits.reduce((total, hit) => total + ((filter === '' || hit.ua.indexOf(filter) === 0) ? hit.count : 0), 0)}
       ]});
   }
 
@@ -82,8 +131,21 @@ function rowSelected(result, category) {
   let barFormatter = new google.visualization.BarFormat({width: 75});
   barFormatter.format(data, 2);
 
-  let table = new google.visualization.Table(document.getElementById('category-table'));
-  table.draw(data, {allowHtml: true});
+  let sorting = {};
+  if (_detailTable) {
+    let oldSort = _detailTable.getSortInfo();
+    if (oldSort) {
+      sorting = {sortAscending: oldSort.ascending, sortColumn: oldSort.column};
+    }
+  }
+
+  _detailTable = new google.visualization.Table(document.getElementById('category-table'));
+  _detailTable.draw(data, {allowHtml: true, ...sorting});
 
   console.log(category, information);
+}
+
+function doSetup() {
+  drawTable();
+  document.getElementById('ua-filter').addEventListener('change', (e) => drawTable(e.target.value));
 }
