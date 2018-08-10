9f6e4b00b18093dd97934a4188a09f5c7c8abb2b
Katharine Berry <ktbry@google.com>
2018-08-10 16:44:22 -0700

    Remove unused backend code

--- dev/e2e-coverage-view/lib/webserver.py
+++ dev/e2e-coverage-view/lib/webserver.py
@@ -1,19 +1,15 @@
 import json
 
-from bottle import route, run, request, response, template, static_file
-
-
-
+from bottle import route, run, request, static_file
 
 # Should really combine with code from exports
 from lib.models import *
-from collections import defaultdict
+
 
 @db_session
 def endpoint_hits():
     endpoints = Endpoint.select(lambda x: x.level == 'stable')
 
-    #return [(endpoint.method, endpoint.url, endpoint.category, count) for (endpoint, count) in hits]
     return [{
         'method': endpoint.method,
         'url': endpoint.url,
@@ -26,15 +22,18 @@ def endpoint_hits():
 def serve_static(filename):
     return static_file(filename, root='www/static')
 
+
 # temporary routes until sunburst is fully integrated
 @route('/conformance/<filename:path>')
 def serve_static(filename):
     return static_file(filename, root='www/conformance')
 
+
 # temporary routes until sunburst is fully integrated
 @route('/')
 def serve_static():
     return static_file('index.html', root='www')
+
 
 # endpoints
 # level,category,method + url,count
@@ -43,11 +42,6 @@ def endpoints_view():
     appname = request.query.get('appname')
     return json.dumps(endpoint_hits())
     #app_usage_endpoints
-
-
-@route('/api/v1/apps')
-def list_apps():
-    return json.dumps(list_apps_names())
 
 
 def start_webserver(host='0.0.0.0', port=9090):
