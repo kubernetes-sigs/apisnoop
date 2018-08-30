e357dea23886d87cf27d61818c0208c50fd6605e
Aaron Crickenberger <spiffxp@google.com>
2018-08-30 16:15:48 -0700

    Port parser.py improvements from e2e-coverage-view
    This will allow audit-log-review to ignore deprecated api endpoints

--- dev/audit-log-review/lib/parsers.py
+++ dev/audit-log-review/lib/parsers.py
@@ -27,6 +27,8 @@ def load_swagger_file(url, cache=False):
             else:
                 # Fall back to old way of doing things
                 key = hashlib.md5(url).hexdigest()
+            if not os.path.exists('cache'):
+                os.mkdir('cache')
             cache_path = os.path.join('cache', "swagger_%s.json" % key)
             if not os.path.exists(cache_path):
                 swagger = requests.get(url).json()
@@ -80,10 +82,18 @@ def load_openapi_spec(url):
         path_data['level'] = parse_level_from_path(path)
         # methods
         path_data['methods'] = {}
-        methods = swagger['paths'][path].keys()
-        for method in methods:
+        for method, swagger_method in swagger['paths'][path].items():
             if method == "parameters":
                 continue
+            if 'deprecated' in swagger_method.get('description', '').lower():
+                print("Skipping DEPRECATED method")
+                continue
+            produces = swagger_method.get('produces', [])
+            can_watch = ('application/json;stream=watch' in produces or
+                         'application/vnd.kubernetes.protobuf;stream=watch' in produces)
+            must_watch = swagger_method.get('x-kubernetes-action') == 'watch'
+            if must_watch:
+                print("must watch " + path)
             method_data = {}
             tags = sorted(swagger['paths'][path][method].get('tags', list()))
             if len(tags) > 0:
@@ -95,7 +105,10 @@ def load_openapi_spec(url):
             else:
                 method_data['category'] = ''
             # todo - request + response
-            path_data['methods'][method] = method_data
+            if not must_watch:
+                path_data['methods'][method] = method_data
+            if can_watch:
+                path_data['methods']['watch'] = method_data
         # use the path regex as the key so that we search for a match easily
         openapi_spec['paths'][path_regex] = path_data
 
@@ -119,10 +132,11 @@ def load_audit_log(path):
             raw_event = json.loads(entry)
             # change verb to represent http request
             verb_tt = {
-                'get': ['get', 'list', 'watch', 'proxy'],
+                'get': ['get', 'list', 'proxy'],
                 'put': ['update', 'patch'],
                 'post': ['create'],
-                'delete': ['delete', 'deletecollection']
+                'delete': ['delete', 'deletecollection'],
+                'watch': ['watch', 'watchlist'],
             }
 
             for method, verbs in verb_tt.items():
