406a9670e8614cbe685979a7bad942c676e68054
Zach! <webmaster@coolguy.website>
2018-10-19 11:23:05 +1300

    sunburst component stateful, connected to redux

---
+++ webui/client/src/reducers/flare-reducer.js
@@ -0,0 +1,12 @@
+const D3FlareData = require('../sample-data/flareData.json')
+
+const initialState = {
+  data: D3FlareData
+}
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+  default:
+    return state
+  }
+}
