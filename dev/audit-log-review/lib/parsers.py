1177a0229bc1908bdb0f4fbc33458866e7069e27
Aaron Crickenberger <spiffxp@google.com>
2018-10-31 08:14:18 -0700

    Improve api coverage related to proxy endpoints
    For both audit-log-review and e2e-coverage-view
      - patch should be its own method
      - redo regex for proxy/ and proxy/{path} endpoints
      - redo logging messages
    
    For e2e-coverage-view:
      - split e2e.test user agent into e2e.test + testcase

--- dev/audit-log-review/lib/parsers.py
+++ dev/audit-log-review/lib/parsers.py
@@ -1,4 +1,3 @@
-
 import re
 import json
 import csv
@@ -44,15 +43,26 @@ def load_swagger_file(url, cache=False):
             swagger = json.load(f)
     return swagger
 
+
+# k8s appears to allow/expect a trailing {path} variable to capture everything
+# remaining in the path, including '/' characters, which doesn't appear to be
+# allowed according to the openapi 2.0 or 3.0 specs
+# (ref: https://github.com/OAI/OpenAPI-Specification/issues/892)
+K8S_PATH_VARIABLE_PATTERN = re.compile("{(path)}$")
 VARIABLE_PATTERN = re.compile("{([^}]+)}")
 def compile_path_regex(path):
+    # first replace the special trailing {path} wildcard with a named regex
+    path_regex = K8S_PATH_VARIABLE_PATTERN.sub("(?P<\\1>.+)", path).rstrip('/')
     # replace wildcards in {varname} format to a named regex
-    path_regex = VARIABLE_PATTERN.sub("(?P<\\1>[^/]+)", path).rstrip('/')
-    if path_regex.endswith("proxy"): # allow for longer paths in proxy
-        path_regex += ".*$"
+    path_regex = VARIABLE_PATTERN.sub("(?P<\\1>[^/]+)", path_regex).rstrip('/')
+    # TODO(spiffxp): unsure if trailing / _should_ be counted toward /proxy
+    if path_regex.endswith("proxy"): # allow proxy to catch a trailing /
+        path_regex += "/?$"
     else:
         path_regex += "$"
+    print 'Converted path: %s into path_regex: %s' % (path, path_regex)
     return path_regex
+
 
 LEVEL_PATTERN = re.compile("/v(?P<api_version>[0-9]+)(?:(?P<api_level>alpha|beta)(?P<api_level_version>[0-9]+))?")
 def parse_level_from_path(path):
@@ -64,6 +74,7 @@ def parse_level_from_path(path):
     if level is None:
         level = "stable"
     return level
+
 
 def load_openapi_spec(url):
     # try:
@@ -86,7 +97,7 @@ def load_openapi_spec(url):
             if method == "parameters":
                 continue
             if 'deprecated' in swagger_method.get('description', '').lower():
-                print("Skipping DEPRECATED method")
+                print 'Skipping deprecated endpoint %s %s' % (method, path)
                 continue
             produces = swagger_method.get('produces', [])
             can_watch = ('application/json;stream=watch' in produces or
@@ -125,6 +136,7 @@ def load_openapi_spec(url):
     #     print("Failed to load openapi spec \"%s\"" % url)
     #     raise e
 
+
 def load_audit_log(path):
     audit_log = []
     with open(path, "rb") as logfile:
@@ -133,7 +145,8 @@ def load_audit_log(path):
             # change verb to represent http request
             verb_tt = {
                 'get': ['get', 'list', 'proxy'],
-                'put': ['update', 'patch'],
+                'patch': ['patch'],
+                'put': ['update'],
                 'post': ['create'],
                 'delete': ['delete', 'deletecollection'],
                 'watch': ['watch', 'watchlist'],
