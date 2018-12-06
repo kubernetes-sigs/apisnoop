dab51fe11f0c5012c902ef791a2d3192b76667bc
Michael Williams <michael.williams@enspiral.com>
2018-12-06 18:07:13 +1300

    Can switch releases and re-load releases
    Choose love <3
    
    Co-Authored-By: Zach! <zz@ii.coop>

--- webui/client/src/bundles/releases.js
+++ webui/client/src/bundles/releases.js
@@ -3,31 +3,27 @@ import { keyBy } from 'lodash'
 
 export default {
   name: 'releases',
+  init: (store) => {
+    store.subscribeToSelectors(
+      ['selectCurrentReleaseName'],
+      (currentReleaseName) => {
+        store.doMarkCurrentReleaseAsOutdated()
+        store.doMarkEndpointsAsOutdated()
+        store.doMarkTestsAsOutdated()
+      }
+    )
+  },
   getReducer: () => {
-    const initialState = {
-      syncdReleaseName: 'master'
-    }
+    const initialState = {}
 
     return (state = initialState, action = {}) => {
-      if (action.type === 'SYNCD_SET_RELEASE_NAME') {
-        return {
-          ...state,
-          syncdReleaseName: action.payload
-        }
-      }
-
       return state;
     }
   },
-  selectSyncdReleaseName: (state) => state.syncdReleaseName,
-  doSyncReleaseName: (releaseName) => ({ dispatch, store }) => {
-    // dispatch({
-    //   type: 'SYNCD_SET_RELEASE_NAME',
-    //   payload: releaseName
-    // })
-
-    // TODO mark all the release resources as stale
-  },
+  //
+  // TODO make doMarkReleaseResourcesAsOutdated()
+  // TODO make selectIsReleaseResourcesFetched()
+  //
   selectCurrentReleaseName: createSelector(
     'selectRouteParams',
     (routeParams) => {
@@ -48,17 +44,5 @@ export default {
   selectReleasesIndexByName: createSelector(
     'selectReleasesIndex',
     releasesIndex => keyBy(releasesIndex, 'name')
-  ),
-  reactShouldSyncReleaseName: createSelector(
-    'selectSyncdReleaseName',
-    'selectCurrentReleaseName',
-    (syncdReleaseName, currentReleaseName) => {
-      if (syncdReleaseName === currentReleaseName) return false
-
-      return {
-        actionCreator: 'doSyncReleaseName',
-        args: [currentReleaseName]
-      }
-    }
   )
 }
