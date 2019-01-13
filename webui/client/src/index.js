c078097d1e2a96e61871e4bea673e9e0579931bc
Zach! Mandeville <zz@ii.coop>
2019-01-14 11:24:51 +1300

    Index.html page shows styled header and footer and loading message

--- webui/client/src/index.js
+++ webui/client/src/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { render } from 'react-dom'
-import './index.css'
+ import { render } from 'react-dom'
+ import './index.css'
 
-import { Provider } from 'redux-bundler-react'
+ import { Provider } from 'redux-bundler-react'
 
-import App from './components/app'
-import createStore from './bundles'
+ import App from './components/app'
+ import createStore from './bundles'
 
-var store = createStore()
+ var store = createStore()
 
-document.title = 'APISnoop | ' // + store.getState().routing.release
+ document.title = 'APISnoop | ' // + store.getState().routing.release
 
 render(
     <Provider store={store}>
-    <App />
-    </Provider>,
-  document.getElementById('root')
+     <App />
+     </Provider>,
+   document.getElementById('root')
 )
