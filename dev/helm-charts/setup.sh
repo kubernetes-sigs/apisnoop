243e711043d9ed27d05237ac4fec33d7397c7646
Hippie Hacker <hh@ii.coop>
2019-01-21 10:36:26 +1300

    Saving dev/helm-charts

--- dev/helm-charts/setup.sh
+++ dev/helm-charts/setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -x -e
 cd $DIR_TEMPLATES
-mkdir $DIR_TEMPLATES/stable
-mkdir $DIR_TEMPLATES/incubator
-git clone https://github.com/helm/charts
+mkdir -p $DIR_TEMPLATES/stable
+mkdir -p $DIR_TEMPLATES/incubator
+git clone --depth 1 https://github.com/helm/charts
 ls charts/stable > stable-apps.txt
 ls charts/incubator > incubator-apps.txt
 tree -L 1
