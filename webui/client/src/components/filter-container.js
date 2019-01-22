1b62cf8224abdd66cd802378f300cb589024872b
Zach! Mandeville <zz@ii.coop>
2019-01-23 10:42:05 +1300

    Add Filter on Endpoint by Text, and other small improvements

--- webui/client/src/components/filter-container.js
+++ webui/client/src/components/filter-container.js
@@ -1,25 +1,22 @@
 import React from 'react'
 import { connect } from 'redux-bundler-react'
 
+import FilterTag from './filter-tag'
+import FilterForm from './filter-form'
+
 function FilterContainer (props) {
   const {
-    doUpdateQuery
+    queryObject
   } = props
 
   return(
-      <section id="filter-container" className="">
-      <button onClick={handleBatchClick}>Filter by Batch</button>
-      <button onClick={handleReset}>Remove Filter</button>
+      <section id="filter-container" className="mb3">
+      {queryObject.filter && <FilterTag filter={queryObject.filter}/>}
+      {!queryObject.filter && <FilterForm />}
       </section>
   )
-  function handleBatchClick () {
-    doUpdateQuery({filter: 'batch'})
-  }
-  function handleReset () {
-    doUpdateQuery({})
-  }
 }
 export default connect(
-  "doUpdateQuery",
+  "selectQueryObject",
   FilterContainer
 )
