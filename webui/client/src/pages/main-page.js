280b8242f2474fc62b0d7eb23a0abbff015f69bf
Zach! Mandeville <zz@ii.coop>
2019-01-23 08:24:39 +1300

    Show Filter Experiment, filter on batch

--- webui/client/src/pages/main-page.js
+++ webui/client/src/pages/main-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'redux-bundler-react'
 
+import FilterContainer from '../components/filter-container'
 import ReleasesContainer from '../components/releases-container'
 import SunburstAndSummary from '../components/sunburst-and-summary'
 import ActiveTestsList from '../components/active-tests-list'
@@ -9,6 +10,7 @@ import ActiveTestSequence from '../components/active-test-sequence'
 function MainPage () {
   return (
       <main id='main-splash' className='min-vh-80 pa4 ma4 flex flex-column'>
+      <FilterContainer />
       <ReleasesContainer />
       <SunburstAndSummary />
       <ActiveTestsList />
