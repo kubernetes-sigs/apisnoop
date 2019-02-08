acfe840d5deb542b97b0329c1a1934a88c3cd379
Hippie Hacker <hh@ii.coop>
2019-02-08 16:06:36 +1300

    Update processArtifacts to use separate output dir

--- apisnoop.sh
+++ apisnoop.sh
@@ -6,9 +6,9 @@ Usage: apisnoop [parameters]
 This utility can fetch and process Kubernetes audit logs.
 
 Parameters:
-  --fetch [source] [destination] Download datasets listed in file. 
+  --fetch [source] [destination] Download datasets listed in file.
 				 By default:
-				    source is ./data-gen/sources.yaml 
+				    source is ./data-gen/sources.yaml
 				    destination is ./data-gen/data.
 				 Both defaults can be overwritten with optional arguments.
 
@@ -18,12 +18,13 @@ EOF
 
 # Our main command line script
 DEFAULT_SOURCE="./data-gen/sources.yaml"
-DEFAULT_DEST="./data-gen/data"
+DEFAULT_STAGING="./data-gen/data"
+DEFAULT_DEST="./data-gen/outdata"
 if [ $# -eq 0 ]; then
   print_help
 elif [ $1 = "--all" ]; then
   echo "Installing necessary dependendies"
-  pip install -r ./data-gen/requirements.txt	
+  pip install -r ./data-gen/requirements.txt
   echo "Fetching latest artifacts"
   ./data-gen/downloadArtifacts.py ${2:-$DEFAULT_SOURCE} ${3:-$DEFAULT_DEST}
   echo "Generating shell script to process artifacts"
@@ -31,12 +32,12 @@ elif [ $1 = "--all" ]; then
   echo "Processing Artifacts"
   bash ./data-gen/processArtifacts.sh
 elif [ $1 = "--fetch" ]; then
-  ./data-gen/downloadArtifacts.py ${2:-$DEFAULT_SOURCE} ${3:-$DEFAULT_DEST}
+  ./data-gen/downloadArtifacts.py ${2:-$DEFAULT_SOURCE} ${3:-$DEFAULT_STAGING}
 elif [ $1 = "--process" ]; then
-  ./data-gen/processArtifacts.py ${2:-$DEFAULT_DEST} > ./data-gen/processArtifacts.sh
+  ./data-gen/processArtifacts.py ${2:-$DEFAULT_STAGING} ${2:-$DEFAULT_DEST} > ./data-gen/processArtifacts.sh
   bash ./data-gen/processArtifacts.sh
 elif [ $1 = "--install" ]; then
-  pip install -r ./data-gen/requirements.txt	
+  pip install -r ./data-gen/requirements.txt
 else
   echo $1 is not a valid flag.  Did you mean --fetch or --process?
 fi
