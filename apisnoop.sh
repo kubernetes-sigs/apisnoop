29a202c58193cf83bf2a8653aad9725f40e7f5f7
Hippie Hacker <hh@ii.coop>
2019-02-08 20:54:38 +1300

    upload/download apisnoop results to gs://apisnoop

--- apisnoop.sh
+++ apisnoop.sh
@@ -6,11 +6,12 @@ Usage: apisnoop [parameters]
 This utility can fetch and process Kubernetes audit logs.
 
 Parameters:
-  --fetch [source] [destination] Download datasets listed in file.
+  --fetch [source] [cache] [destination] Download datasets listed in file.
 				 By default:
 				    source is ./data-gen/sources.yaml
-				    destination is ./data-gen/data.
-				 Both defaults can be overwritten with optional arguments.
+				    cache is ./data-gen/cache
+				    destination is ./data-gen/data
+				 Defaults can be overwritten with optional arguments.
 
   --process           Process datasets
 EOF
@@ -18,26 +19,36 @@ EOF
 
 # Our main command line script
 DEFAULT_SOURCE="./data-gen/sources.yaml"
-DEFAULT_STAGING="./data-gen/data"
-DEFAULT_DEST="./data-gen/outdata"
+DEFAULT_CACHE="./data-gen/cache"
+DEFAULT_DEST="./data-gen/processed"
+DEFAULT_GCS_PREFIX="gs://apisnoop/dev/"
+
 if [ $# -eq 0 ]; then
   print_help
 elif [ $1 = "--all" ]; then
   echo "Installing necessary dependendies"
   pip install -r ./data-gen/requirements.txt
   echo "Fetching latest artifacts"
-  ./data-gen/downloadArtifacts.py ${2:-$DEFAULT_SOURCE} ${3:-$DEFAULT_DEST}
+  ./data-gen/downloadArtifacts.py ${2:-$DEFAULT_SOURCE} ${3:-$DEFAULT_CACHE}
   echo "Generating shell script to process artifacts"
-  ./data-gen/processArtifacts.py ${2:-$DEFAULT_DEST} > ./data-gen/processArtifacts.sh
+  ./data-gen/processArtifacts.py ${3:-$DEFAULT_CACHE} ${4:-$DEFAULT_DEST} > ./data-gen/processArtifacts.sh
   echo "Processing Artifacts"
   bash ./data-gen/processArtifacts.sh
-elif [ $1 = "--fetch" ]; then
-  ./data-gen/downloadArtifacts.py ${2:-$DEFAULT_SOURCE} ${3:-$DEFAULT_STAGING}
-elif [ $1 = "--process" ]; then
-  ./data-gen/processArtifacts.py ${2:-$DEFAULT_STAGING} ${2:-$DEFAULT_DEST} > ./data-gen/processArtifacts.sh
+elif [ $1 = "--update-sources" ]; then
+  ./data-gen/updateSources.py ./data-gen/sources.yaml
+elif [ $1 = "--update-cache" ]; then
+  ./data-gen/downloadArtifacts.py ${2:-$DEFAULT_SOURCE} ${3:-$DEFAULT_CACHE}
+elif [ $1 = "--process-cache" ]; then
+  ./data-gen/processArtifacts.py ${2:-$DEFAULT_CACHE} ${3:-$DEFAULT_DEST} > ./data-gen/processArtifacts.sh
   bash ./data-gen/processArtifacts.sh
+elif [ $1 = "--upload-apiusage" ]; then
+  cd $DEFAULT_DEST
+  gsutil -m cp -R -n ./ $DEFAULT_GCS_PREFIX
+elif [ $1 = "--download-apiusage" ]; then
+  mkdir -p $DEFAULT_DEST
+  gsutil -m cp -R -n $DEFAULT_GCS_PREFIX $DEFAULT_DEST
 elif [ $1 = "--install" ]; then
   pip install -r ./data-gen/requirements.txt
 else
-  echo $1 is not a valid flag.  Did you mean --fetch or --process?
+  echo $1 is not a valid flag.  Did you mean --fetch, --update or --process?
 fi
