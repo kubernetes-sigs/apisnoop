99903418d8dbe644da2351ee2f1cdcb48e4304d8
Hippie Hacker <hh@ii.coop>
2019-02-08 21:01:20 +1300

    Update apisnoop.sh with more precise parameter info

--- apisnoop.sh
+++ apisnoop.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
-print_help() {
-	cat << EOF
-Usage: apisnoop [parameters]
-This utility can fetch and process Kubernetes audit logs.
-
-Parameters:
-  --fetch [source] [cache] [destination] Download datasets listed in file.
-				 By default:
-				    source is ./data-gen/sources.yaml
-				    cache is ./data-gen/cache
-				    destination is ./data-gen/data
-				 Defaults can be overwritten with optional arguments.
-
-  --process           Process datasets
-EOF
-}
-
 # Our main command line script
 DEFAULT_SOURCE="./data-gen/sources.yaml"
 DEFAULT_CACHE="./data-gen/cache"
 DEFAULT_DEST="./data-gen/processed"
 DEFAULT_GCS_PREFIX="gs://apisnoop/dev/"
+
+print_help() {
+	cat << EOF
+Usage: apisnoop [parameters]
+This utility can fetch and process Kubernetes audit logs.
+      By default:
+        source is $DEFAULT_SOURCE
+        cache is $DEFAULT_CACHE
+        destination is $DEFAULT_DEST
+        gcs_prefix is $DEFAULT_GCS_PREFIX
+      Defaults can be overwritten with optional arguments.
+
+Parameters:
+  --fetch [source] [cache] [destination] Download datasets listed in file.
+  --update-sources    Check for latest successful jobs and update sources.yaml
+  --update-cache      Download raw audit-logs based on sources.yaml
+  --process-cache     Process raw audit-logs and save apiusage results to disk
+  --upload-apiusage   Upload apiusage results to a gcs bucket
+  --download-apiusage Download apiusage results from gcs bucket
+EOF
+}
 
 if [ $# -eq 0 ]; then
   print_help
