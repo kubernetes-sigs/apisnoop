839e2bacbbc22f4bbfaa8844e8c3031cd05d431c
Zach Mandeville <zz@ii.coop>
2019-03-04 23:41:30 +0000

    refactor for better-reading code

--- web/backend/src/services/config/config.class.js
+++ web/backend/src/services/config/config.class.js
@@ -8,28 +8,33 @@ class Service {
   }
 
   async setup (app, params) {
-    populateReleases(app,'../../data-gen/processed')
+    populateAPI(app,'../../data-gen/processed')
   }
 }
-function populateReleases (app, dir)  {
+function populateAPI (app, dir)  {
   var globOpts = {
     cwd: dir
   }
+  populateEndpointsAndTestsAndUseragents(app, globOpts, dir)
+  populateReleases(app, globOpts, dir)
+}
 
-  glob("**/apisnoop.json", globOpts, (err, processedAudits) => {
+function populateEndpointsAndTestsAndUseragents (app, opts, dir) {
+  glob("**/apisnoop.json", opts, (err, processedAudits) => {
     for (var i = 0; i < processedAudits.length; i++) {
       var fileName = processedAudits[i]
       var releaseJson = fs.readFileSync(`${dir}/${fileName}`, 'utf-8')
       var releaseData = JSON.parse(releaseJson)
-      var metadataName =  fileName.replace('apisnoop.json', 'metadata.json')
-      var metadataJson = fs.readFileSync(`${dir}/${metadataName}`, 'utf-8')
-      var metadata = JSON.parse(metadataJson)
       var bucketJobRelease = getBucketJobReleaseFrom(fileName)
       addEntryToEndpointService(app, releaseData, bucketJobRelease)
       addEntryToTestService(app, releaseData, bucketJobRelease)
       addEntryToUseragentsService(app, releaseData, bucketJobRelease)
     }
   })
+}
+
+function populateReleases (app, opts, dir) {
+  console.log('release Stuff Goes Here')
 }
 
 function getBucketJobReleaseFrom (fileName) {
@@ -42,83 +47,83 @@ function getBucketJobReleaseFrom (fileName) {
 }
 
 async function addEntryToEndpointService (app, releaseData, bucketJobRelease) {
-    var service = app.service('/api/v1/endpoints')
-    var endpointNames = Object.keys(releaseData.endpoints)
-    var tests = releaseData.tests
-    for (var endpointName of endpointNames) {
-        var endpointMethods = Object.keys(releaseData.endpoints[endpointName])
-        for (var endpointMethod of endpointMethods) {
-            var rawEndpoint = releaseData.endpoints[endpointName][endpointMethod]
-            var endpoint = {
-                name: endpointName,
-                method: endpointMethod,
-                level: rawEndpoint.level,
-                test_tags: rawEndpoint.test_tags,
-                tests: rawEndpoint.tests,
-                description: rawEndpoint.desc,
-                path: rawEndpoint.path,
-                category: rawEndpoint.cat,
-                isTested: rawEndpoint.counter > 0,
-                ...bucketJobRelease
-            }
-            // An endpoint is unique by name, release, method.
-            var uniqueQuery = {
-                name: endpoint.name,
-                method: endpoint.method,
-                release: endpoint.release
-            }
-            addOrUpdateEntry(service, endpoint, uniqueQuery)
-        }
+  var service = app.service('/api/v1/endpoints')
+  var endpointNames = Object.keys(releaseData.endpoints)
+  var tests = releaseData.tests
+  for (var endpointName of endpointNames) {
+    var endpointMethods = Object.keys(releaseData.endpoints[endpointName])
+    for (var endpointMethod of endpointMethods) {
+      var rawEndpoint = releaseData.endpoints[endpointName][endpointMethod]
+      var endpoint = {
+        name: endpointName,
+        method: endpointMethod,
+        level: rawEndpoint.level,
+        test_tags: rawEndpoint.test_tags,
+        tests: rawEndpoint.tests,
+        description: rawEndpoint.desc,
+        path: rawEndpoint.path,
+        category: rawEndpoint.cat,
+        isTested: rawEndpoint.counter > 0,
+        ...bucketJobRelease
+      }
+      // An endpoint is unique by name, release, method.
+      var uniqueQuery = {
+        name: endpoint.name,
+        method: endpoint.method,
+        release: endpoint.release
+      }
+      addOrUpdateEntry(service, endpoint, uniqueQuery)
     }
+  }
 }
 
 async function addEntryToTestService (app, releaseData, bucketJobRelease) {
-    var service = app.service('/api/v1/tests')
-    var testNames = Object.keys(releaseData.test_sequences)
-    for (var testName of testNames) {
-        var testSequence = releaseData.test_sequences[testName]
-        var test = {
-            name: testName,
-          sequence: testSequence,
-          ...bucketJobRelease
+  var service = app.service('/api/v1/tests')
+  var testNames = Object.keys(releaseData.test_sequences)
+  for (var testName of testNames) {
+    var testSequence = releaseData.test_sequences[testName]
+    var test = {
+      name: testName,
+      sequence: testSequence,
+      ...bucketJobRelease
 
-        }
-        // An test is unique by testName and Release.
-        var uniqueQuery = {
-            name: test.name,
-            release: test.release
-        }
-        addOrUpdateEntry(service, test, uniqueQuery)
     }
+    // An test is unique by testName and Release.
+    var uniqueQuery = {
+      name: test.name,
+      release: test.release
+    }
+    addOrUpdateEntry(service, test, uniqueQuery)
+  }
 }
 
 async function addEntryToUseragentsService (app, releaseData, bucketJobRelease) {
-    var service = app.service('/api/v1/useragents')
-    var useragents = Object.keys(releaseData.useragents)
-    for (var useragentEntry of useragents) {
-        var touchedEndpoints = releaseData
-        var touchedEndpoints = releaseData.useragents[useragentEntry]
-        var useragent = {
-            name: useragentEntry,
-            endpoints: touchedEndpoints,
-            ...bucketJobRelease
-        }
-        // A useragent is unique by Name and Release.
-        var uniqueQuery = {
-            name: useragent.name,
-            release: useragent.release
-        }
-        addOrUpdateEntry(service, useragent, uniqueQuery)
+  var service = app.service('/api/v1/useragents')
+  var useragents = Object.keys(releaseData.useragents)
+  for (var useragentEntry of useragents) {
+    var touchedEndpoints = releaseData
+    var touchedEndpoints = releaseData.useragents[useragentEntry]
+    var useragent = {
+      name: useragentEntry,
+      endpoints: touchedEndpoints,
+      ...bucketJobRelease
     }
+    // A useragent is unique by Name and Release.
+    var uniqueQuery = {
+      name: useragent.name,
+      release: useragent.release
+    }
+    addOrUpdateEntry(service, useragent, uniqueQuery)
+  }
 }
 
 async function addOrUpdateEntry (service, entry, uniqueQuery) {
-    var existingEntry = await service.find({query:uniqueQuery})
-    if (existingEntry.length === 0) {
-        await service.create(entry)
-    } else {
-        await service.update(existingEntry[0]._id, entry)
-    }
+  var existingEntry = await service.find({query:uniqueQuery})
+  if (existingEntry.length === 0) {
+    await service.create(entry)
+  } else {
+    await service.update(existingEntry[0]._id, entry)
+  }
 }
 
 
