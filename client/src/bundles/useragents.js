5a6fa617e6b399b7357aded4e378a43314c95565
Zach! Mandeville <zz@ii.coop>
2019-03-19 16:22:17 +1300

    Add selected useragent names based on input

--- client/src/bundles/useragents.js
+++ client/src/bundles/useragents.js
@@ -1,15 +1,38 @@
-export default {
-  name: 'useragents',
-  getReducer: () => {
-      const initialState = {
-          filterInput: ''
-      }
-      return (state=initialState, {type, payload}) => {
-          if (type  === 'USERAGENT_FILTER_INPUT_UPDATED') {
-              return {...state, filterInput: payload}
-          }
-          return state
-      }
-  },
-  selectUseragentsInput: (state) => state.useragents.filterInput
-}
+import { map } from 'lodash'
+import { createSelector } from 'redux-bundler'
+  export default {
+    name: 'useragents',
+    getReducer: () => {
+        const initialState = {
+            filterInput: ''
+        }
+        return (state=initialState, {type, payload}) => {
+            if (type  === 'USERAGENT_INPUT_UPDATED') {
+                return {...state, filterInput: payload}
+            }
+            return state
+        }
+    },
+    selectUseragentsInput: (state) => state.useragents.filterInput,
+    selectUseragentsFilteredByInput: createSelector(
+        'selectUseragentsResource',
+        'selectUseragentsInput',
+        (useragents, input) => {
+            var useragentsNames = map(useragents, 'name')
+            if (input === '') return useragentsNames
+    
+            return useragentsNames.filter(ua => {
+                var inputAsRegex = new RegExp(input)
+                return inputAsRegex.test(ua)
+            })
+        }
+    )
+    ,
+    doUpdateUseragentInput: (payload) => ({dispatch}) => {
+        dispatch({
+            type: 'USERAGENT_INPUT_UPDATED',
+            payload
+        })
+    }
+    
+  }
