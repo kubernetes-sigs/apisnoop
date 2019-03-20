8f3e11a176af37a93b2fe8d0cc7797cd29558094
Zach! Mandeville <zz@ii.coop>
2019-03-20 14:25:47 +1300

    filtered endpoint selector added

--- client/src/bundles/useragents.js
+++ client/src/bundles/useragents.js
@@ -1,53 +1,82 @@
-import { filter, map } from 'lodash'
+import { filter,
+         flattenDeep,
+         map,
+         uniq } from 'lodash'
 import { createSelector } from 'redux-bundler'
-  export default {
-    name: 'useragents',
-    getReducer: () => {
-        const initialState = {
-            filterInput: ''
-        }
-        return (state=initialState, {type, payload}) => {
-            if (type  === 'USERAGENT_INPUT_UPDATED') {
-                return {...state, filterInput: payload}
+export default {
+  name: 'useragents',
+  getReducer: () => {
+      const initialState = {
+          filterInput: ''
+      }
+      return (state=initialState, {type, payload}) => {
+          if (type  === 'USERAGENT_INPUT_UPDATED') {
+              return {...state, filterInput: payload}
+          }
+          return state
+      }
+  },
+  selectUseragentsInput: (state) => state.useragents.filterInput,
+  selectUseragentsFilteredByInput: createSelector(
+      'selectUseragentsResource',
+      'selectUseragentsInput',
+      (useragents, input) => {
+          var useragentsNames = map(useragents, 'name')
+          if (input === '') return useragentsNames
+  
+          return useragentsNames.filter(ua => {
+              var inputAsRegex = new RegExp(input)
+              return inputAsRegex.test(ua)
+          })
+      }
+  )
+  ,
+  selectUseragentsFilteredByQuery: createSelector(
+      'selectUseragentsResource',
+      'selectQueryObject',
+      (useragents, query) => {
+          if (useragents == null) return null
+          if (query.useragent && query.useragent.length) {
+              return filter(useragents, (ua) => {
+                  var inputAsRegex = new RegExp(query.useragent)
+                  return inputAsRegex.test(ua.name)
+              })
+          } else {
+              return useragents
+          }
+      }
+  ),
+  selectEndpointsHitByFilteredUseragents: createSelector(
+    'selectUseragentsFilteredByQuery',
+    (useragents) => {
+      if (useragents == null) return null
+      var endpointsHit = []
+      var uaEndpointsOnly = map(useragents, "endpoints")
+      for (var uaEntry of uaEndpointsOnly) {
+        var endpointNames = Object.keys(uaEntry)
+        for (var endpoint of endpointNames) {
+          var methods = Object.keys(uaEntry[endpoint])
+          for (var method of methods) {
+            var useragent = useragents[uaEndpointsOnly.indexOf(uaEntry)]
+            var endpointEntry = {
+              name: endpoint,
+              method: method,
+              release: useragent.release,
+              bucket: useragent.bucket,
+              job: useragent.job
             }
-            return state
+            endpointsHit.push(endpointEntry)
+          }
         }
-    },
-    selectUseragentsInput: (state) => state.useragents.filterInput,
-    selectUseragentsFilteredByInput: createSelector(
-        'selectUseragentsResource',
-        'selectUseragentsInput',
-        (useragents, input) => {
-            var useragentsNames = map(useragents, 'name')
-            if (input === '') return useragentsNames
-    
-            return useragentsNames.filter(ua => {
-                var inputAsRegex = new RegExp(input)
-                return inputAsRegex.test(ua)
-            })
-        }
-    )
-    ,
-    selectUseragentsFilteredByQuery: createSelector(
-        'selectUseragentsResource',
-        'selectQueryObject',
-        (useragents, query) => {
-            if (useragents == null) return null
-            if (query.useragent && query.useragent.length) {
-                return filter(useragents, (ua) => {
-                    var inputAsRegex = new RegExp(query.useragent)
-                    return inputAsRegex.test(ua.name)
-                })
-            } else {
-                return useragents
-            }
-        }
-    ),
-    doUpdateUseragentsInput: (payload) => ({dispatch}) => {
-        dispatch({
-            type: 'USERAGENT_INPUT_UPDATED',
-            payload
-        })
+      }
+      return uniq(endpointsHit)
     }
-    
+  ),
+  doUpdateUseragentsInput: (payload) => ({dispatch}) => {
+      dispatch({
+          type: 'USERAGENT_INPUT_UPDATED',
+          payload
+      })
   }
+  
+}
