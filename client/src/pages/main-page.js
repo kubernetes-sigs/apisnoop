6cc06293bca4de287073a034ed12bec67e2ace76
Zach! Mandeville <zz@ii.coop>
2019-03-26 15:11:01 +1300

    Add useragent searchbar

--- client/src/pages/main-page.js
+++ client/src/pages/main-page.js
@@ -3,6 +3,7 @@ import { connect } from 'redux-bundler-react'
 
 // import FilterContainer from '../components/filter-container' # a regex filter for endpoints.
 import ReleasesContainer from '../components/releases-container'
+import UseragentSearchContainer from '../components/useragent-search-container'
 import SunburstAndSummary from '../components/sunburst-and-summary'
 import ActiveTestsList from '../components/active-tests-list'
 import ActiveTestSequence from '../components/active-test-sequence'
@@ -11,6 +12,7 @@ function MainPage () {
     return (
       <main id='main-splash' className='min-vh-80 pa4 ma4 flex flex-column'>
       {/*<FilterContainer />*/}
+      <UseragentSearchContainer />
       <ReleasesContainer />
       <SunburstAndSummary />
       <ActiveTestsList />
