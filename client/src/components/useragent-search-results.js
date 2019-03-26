efa4ad167eee7a408bb607b0527db2399a4ded63
Zach! Mandeville <zz@ii.coop>
2019-03-26 15:52:04 +1300

    Add List of useragents covered by active filter

--- client/src/components/useragent-search-results.js
+++ client/src/components/useragent-search-results.js
@@ -5,9 +5,12 @@ function UseragentSearchResults (props) {
   const useragents = props.useragentsFilteredByInput
   if(useragents == null || useragents.length <= 0) return null
   return (
-      <ul>
-        {useragents.map(useragent => <li>{ useragent }</li>)}
+    <div id='useragent-search-results' className='mt2 mb0'>
+      <strong className='f5'>Useragents covered by this regex</strong>
+      <ul className="list ph0">
+        {useragents.map(useragent => <li key={useragent} className="f6 dib mr2 pa2 mid-gray">{ useragent }</li>)}
       </ul>
+    </div>
   )
 }
 
