3f90717d36ede7fc87d3d2b0352e0738f6609368
Zach! Mandeville <zz@ii.coop>
2019-03-28 15:34:41 +1300

    route of '/' redirects to master release master available
    Co-Authored-By: Hippie Hacker <hh@ii.coop>
    Co-Authored-By: Devan Carpenter <devan@ii.coop>

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -6,10 +6,16 @@ import Header from './header'
 import Footer from './footer'
 
 export default connect(
+  'selectMasterRelease',
+  'selectRouteInfo',
   'doUpdateUrl',
   'selectRoute',
-  ({ doUpdateUrl, route }) => {
+  ({ masterRelease, routeInfo, doUpdateUrl, doReplaceUrl, route }) => {
     const CurrentPage = route
+    if (masterRelease && routeInfo.url === '/') {
+      doUpdateUrl(`/${masterRelease.name}`)
+      return null
+    }
     return (
         <div onClick={navHelper(doUpdateUrl)}>
         <Header />
