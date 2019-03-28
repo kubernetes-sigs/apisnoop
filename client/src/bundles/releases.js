489fd05af0b9ebda344c3a76095411d053e40681
Zach! Mandeville <zz@ii.coop>
2019-03-28 14:05:21 +1300

    Mark useragents as outdated when release changes
    Co-Authored-By: Hippie Hacker <hh@ii.coop>

--- client/src/bundles/releases.js
+++ client/src/bundles/releases.js
@@ -14,6 +14,7 @@ export default {
         store.doMarkCurrentReleaseAsOutdated()
         store.doMarkEndpointsResourceAsOutdated()
         store.doMarkTestsResourceAsOutdated()
+        store.doMarkUseragentsResourceAsOutdated()
       }
     )
   },
