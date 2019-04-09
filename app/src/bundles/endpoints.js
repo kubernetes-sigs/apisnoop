9cefa511f8f35addea5d1c26c5cf1598b453ebef
Zach! Mandeville <zz@ii.coop>
2019-04-09 15:06:35 +1200

    Endpoints added to Store and Sorted by Level, Category, OpID
    This is in preparation for our sunburst, using the new data structure and now
    much smaller code.  I am including the endpoints.json now, but should be removed
    in later commits as it should be generated as part of build process.

--- app/src/bundles/endpoints.js
+++ app/src/bundles/endpoints.js
@@ -1,9 +1,36 @@
+import { createSelector } from 'redux-bundler'
+import { groupBy, mapValues, transform } from 'lodash'
+
+import endpoints from '../data/endpoints.json'
+
 export default {
   name: 'endpoints',
-  getReducer: () => {
-    const initialState = []
-    return (state=initialState, action) => {
-      return state
-    }
-  }
+    getReducer: () => {
+      const initialState = endpoints
+      return (state=initialState, action) => {
+        return state
+      }
+    },
+    selectEndpoints: (state) => state.endpoints,
+    selectEndpointsByLevelAndCategoryAndOperatorId: createSelector(
+      'selectEndpoints',
+      (endpoints) => {
+        var endpointsWithOpIds = mapValues (endpoints, (value, key, endpoints) => {
+          return {operatorId: key, ...value}
+        })
+        var endpointsByLevel = groupBy(endpointsWithOpIds, 'level')
+        return mapValues(endpointsByLevel, endpointsInLevel => {
+          var endpointsByCategory = groupBy(endpointsInLevel, 'category')
+          return mapValues(endpointsByCategory, endpointsInCategory => {
+            return endpointsInCategory.map (endpoint => {
+              return {
+                ...endpoint,
+                size: 1
+              }
+            })
+          })
+        })
+      }
+    )
+    
 }
