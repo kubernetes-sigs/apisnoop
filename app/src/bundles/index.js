91d5b4c3f765b51ba04f0418031c2f496521bba6
Zach! Mandeville <zz@ii.coop>
2019-04-10 10:00:25 +1200

    Add colours bundle and beginning work of sunburst bundle

--- app/src/bundles/index.js
+++ app/src/bundles/index.js
@@ -1,8 +1,12 @@
 import { composeBundles } from 'redux-bundler'
+
 import jobResultsMetadata from './job-results-metadata'
 import endpoints from './endpoints'
+import colours from './colours'
+
 
 export default composeBundles(
+  colours,
   endpoints,
   jobResultsMetadata
 )
