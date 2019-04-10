32e4b8adea2370ecb8635fb0f1dc3419a5525ba9
Zach! Mandeville <zz@ii.coop>
2019-04-10 14:57:38 +1200

    Add Sunburst Header back to sunburst container

--- app/src/pages/main-page.js
+++ app/src/pages/main-page.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { connect } from 'redux-bundler-react'
 
 // import FilterContainer from '../components/filter-container' # a regex filter for endpoints.
-import Metadata from '../components/metadata'
-import Sunburst from '../components/sunburst'
+import Sunburst from '../components/sunburst-container'
 // import UseragentSearchContainer from '../components/useragent-search-container'
 // import SunburstAndSummary from '../components/sunburst-and-summary'
 // import ActiveTestsList from '../components/active-tests-list'
@@ -12,7 +11,6 @@ import Sunburst from '../components/sunburst'
 function MainPage () {
   return (
     <main id='main-splash' className='min-vh-80 pa4 ma4 flex flex-column'>
-      <Metadata />
       <h1>HELLO YOU ARE DOING GOODLY</h1>
       <Sunburst />
       {/*<FilterContainer />*/}
