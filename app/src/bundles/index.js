ed7eef6b28be743ea7f28d6000dddcb96b7b944b
Zach! Mandeville <zz@ii.coop>
2019-04-16 15:03:47 +1200

    Update core bundle with new bundles, update app.org with new changes

--- app/src/bundles/index.js
+++ app/src/bundles/index.js
@@ -8,10 +8,13 @@ import endpointsResource from './endpoints-resource'
 import metadataResource from './metadata-resource'
 import testsResource from './tests-resource'
 import testTagsResource from './test-tags-resource'
+import testSequencesResource from './test-sequences-resource'
 import useragentsResource from './useragents-resource'
 import jobResultsMetadata from './job-results-metadata'
 import sunburst from './sunburst'
 import testedStats from './tested-stats'
+import tests from './tests'
+import testTags from './test-tags'
 import useragents from './useragents'
 import zoom from './zoom'
 
@@ -24,10 +27,13 @@ export default composeBundles(
   metadataResource,
   testsResource,
   testTagsResource,
+  testSequencesResource,
   useragentsResource,
   jobResultsMetadata,
   sunburst,
   testedStats,
+  tests,
+  testTags,
   useragents,
   zoom
 )
