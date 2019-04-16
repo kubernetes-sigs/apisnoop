2493d29dba2b98b16298d669760acb35fc3184dc
Zach! Mandeville <zz@ii.coop>
2019-04-16 15:23:48 +1200

    Functionality built for test tag filter

---
+++ app/src/bundles/tests.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'redux-bundler'
+import { pickBy } from 'lodash'
+
+export default {
+  name: 'tests',
+  selectActiveTests: createSelector(
+    'selectActiveEndpoint',
+    'selectTestsResource',
+    (endpoint, tests) => {
+      let activeTests = []
+      if (endpoint == null || tests== null) return activeTests
+      else {
+        activeTests = pickBy(tests, (test) => test.includes(endpoint.operationId))
+        return Object.keys(activeTests)
+      }
+    }
+  )
+}
---
+++ app/src/components/test-tags-list.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'redux-bundler-react'
+
+function TestTag (props) {
+  const { testTag } = props
+  return (
+    <li className='dib tal ttsc mr2 mb2 ph2 pv1 mid-gray'>
+      { testTag }
+    </li>
+  )
+}
+
+function TestTagsList (props) {
+  const { activeTestTags } = props
+
+  if (activeTestTags == null) return null
+  if (activeTestTags.length === 0) return null
+
+  return (
+    <div className="mt4 pl0">
+      <p>Tests are coming from:</p>
+      <div className="scrollbox">
+      <ul className='list pl0 ml0'>
+      {activeTestTags.map((testTag, index) => {
+          return <TestTag key={'testTag_'.concat(index)} testTag={ testTag } />
+        })}
+      </ul>
+    </div>
+    </div>
+  )
+}
+
+export default connect(
+  'selectActiveTestTags',
+  TestTagsList
+)
