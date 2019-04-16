26e08f4385b4b5111a3832124db93127f301c576
Zach! Mandeville <zz@ii.coop>
2019-04-16 15:01:54 +1200

    Correct Errors raised with active Location

--- app/src/bundles/active-location.js
+++ app/src/bundles/active-location.js
@@ -2,7 +2,7 @@ import { isEmpty } from 'lodash'
 import { createSelector } from 'redux-bundler'
 
 export default {
-  name: 'colours',
+  name: 'activeLocation',
   selectActiveLocation: createSelector(
     'selectQueryObject',
     'selectZoom',
@@ -34,33 +34,3 @@ export default {
   )
   
 }
-
-selectActiveLocation: createSelector(
-  'selectQueryObject',
-  'selectZoom',
-  (query, zoom) => {
-    let activeLocation = {
-      level: '',
-      category: '',
-      operationId: ''
-    }
-    if (isEmpty(query) && zoom == null) {
-      return activeLocation
-    }
-    if (!isEmpty(query) && query.level) {
-      activeLocation.level = query.level
-      activeLocation.category = query.category || ''
-      activeLocation.operationId = query.operationId || ''
-      return activeLocation
-    }
-    if (!query.level && zoom.level) {
-      activeLocation.level = zoom.level
-      activeLocation.category = zoom.category || ''
-      activeLocation.operationId = zoom.operationId || ''
-      return activeLocation
-    }
-    else {
-      return activeLocation
-    }
-  }
-)
