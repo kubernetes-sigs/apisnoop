b10b493202b9168314af5d59687ed2b58e283bde
Zach! Mandeville <zz@ii.coop>
2019-04-16 16:49:08 +1200

    Made Searchbar generic, added search by useragent and test_tags

---
+++ app/src/components/search-bar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'redux-bundler-react'
+import { pickBy } from 'lodash'
+
+function SearchBar (props) {
+  const {
+    doUpdateInput,
+    doUpdateQuery,
+    input,
+    searchFilter,
+    queryObject } = props
+
+  return (
+      <form onSubmit={handleSubmit}>
+      <input name='ua-filter' type='text' value={input} onChange={handleInput}/>
+      <input type='submit' value='submit' />
+      </form>
+      )
+
+  function handleInput (e) {
+    doUpdateInput(e.target.value)
+  }
+  function handleSubmit (e) {
+    e.preventDefault()
+    let filters = {
+      useragents: queryObject.useragents,
+      test_tags: queryObject.test_tags
+    }
+    filters[searchFilter] = e.target[0].value
+    filters = pickBy(filters, (v,k) => v !== '' && v !== undefined)
+    doUpdateQuery({...filters})
+    doUpdateInput('')
+  }
+}
+
+export default connect(
+  'doUpdateQuery',
+  'selectQueryObject',
+  SearchBar
+)
