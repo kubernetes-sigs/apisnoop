fc85dcf538c9866f3dadd862e36cf1918245dfcc
Zach! Mandeville <zz@ii.coop>
2019-04-24 11:25:42 +1200

    Add Bucketlist As initial View
    Co-Authored-By: Hippie Hacker <hh@ii.coop>
    Co-Authored-By: Devan Carpenter <devan@ii.coop>

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -4,17 +4,36 @@ import { connect } from 'redux-bundler-react'
 
 import Header from './header'
 import Footer from './footer'
+import BucketList from './bucket-list'
 import MainPage from '../pages/main-page'
 
-export default connect(
- 'doUpdateUrl',
-  ({doUpdateUrl}) => {
+function App (props) {
+  const {
+    doUpdateUrl,
+    queryObject
+  } = props
+
+  if (queryObject && queryObject.bucket) {
     return (
         <div onClick={navHelper(doUpdateUrl)}>
         <Header />
-        <MainPage />
+       <MainPage />
+        <Footer />
+        </div>
+    )
+  } else {
+    return(
+        <div onClick={navHelper(doUpdateUrl)}>
+        <Header />
+        <BucketList />
         <Footer />
         </div>
     )
   }
+}
+
+export default connect(
+ 'doUpdateUrl',
+ 'selectQueryObject',
+  App
 )
