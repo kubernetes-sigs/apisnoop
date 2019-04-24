c3fe428c1c9cd43862020ee40399e3db13fbcfd5
Zach! Mandeville <zz@ii.coop>
2019-04-24 15:02:19 +1200

    Sunburst header shows accurate job-version and bucket

--- app/src/components/sunburst-header.js
+++ app/src/components/sunburst-header.js
@@ -3,21 +3,23 @@ import { connect } from 'redux-bundler-react'
 
 const SunburstHeader = (props) => {
   const {
-    metadata
+    job,
+    bucket
   } = props
 
+  if (job.length === 0) return null
   return (
       <div id='sunburst-header' className='relative'>
       <h2 className='mb1 mt1 pt4 f1'>
-      { metadata.version }
+      { job }
       </h2>
-      <p className='ibm-plex-mono f6 mt0 pt0 pl2'> from job {metadata.job} in bucket {metadata.bucket}</p>
-      <p className='ibm-plex-mono f6 mb1 pt0 pl2'>Data Gathered on {metadata.timestamp}</p>
+      <p className='ibm-plex-mono f6 mt0 pt0 pl2'> from {bucket}</p>
       </div>
   )
 }
 
 export default connect(
-  'selectMetadata',
+  'selectBucket',
+  'selectJob',
   SunburstHeader
 )
