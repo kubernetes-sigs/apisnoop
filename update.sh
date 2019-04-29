80006a7cd870db809ad964a4086c4034866cc8d0
Zach! Mandeville <zz@ii.coop>
2019-04-30 10:38:17 +1200

    audit-sources: gs-bucket now coming from update.sh result
    Co-Authored-By: Devan Carpenter <devan@ii.coop>

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ env
 curl https://storage.googleapis.com/pub/gsutil.tar.gz | tar xfz -
 export PATH=$PWD/gsutil:$PATH
 JOBID=$(gsutil cat gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/latest-build.txt)
-NEW_BUCKET="zz-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOBID/artifacts/"
+NEW_BUCKET="gs-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOBID/artifacts/"
 echo $NEW_BUCKET >> audit-sources.yaml
+cat audit-sources.yaml
 cp audit-sources.yaml app/public/audit-sources.yaml
