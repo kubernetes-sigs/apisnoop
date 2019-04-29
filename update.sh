70e7bb9657b92165b1b85d88060ef92f617949bb
Zach! Mandeville <zz@ii.coop>
2019-04-30 10:11:51 +1200

    test update.sh script w netlify

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ set -x
 set -e
 env
 JOBID=$(gsutil cat gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/latest-build.txt)
-NEW_BUCKET=apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOBID/artifacts/
+NEW_BUCKET="zz-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOBID/artifacts/"
 echo $NEW_BUCKET >> audit-sources.yaml
 cp audit-sources.yaml app/public/audit-sources.yaml
