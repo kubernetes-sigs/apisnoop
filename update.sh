94855ca11f6fb2585c0ad647ce2e3adb27aeb641
Zach! Mandeville <zz@ii.coop>
2019-04-29 15:22:45 +1200

    add update.sh template

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://www.netlify.com/docs/continuous-deployment/#environment-variables
+set -x
+set -e
+env
+JOBID=$(gsutil cat gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/latest-build.txt)
+NEW_BUCKET=apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOBID/artifacts/
+echo $NEW_BUCKET >> audit-sources.yaml
+cp audit-sources.yaml app/public/audit-sources.yaml
