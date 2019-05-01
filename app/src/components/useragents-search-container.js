81f6150da56ff7d38eb78101aa22fb343b6b142d
Zach! Mandeville <zz@ii.coop>
2019-05-01 12:57:54 +1200

    Begin style exploration for filter containers

--- app/src/components/useragents-search-container.js
+++ app/src/components/useragents-search-container.js
@@ -10,20 +10,42 @@ function UseragentsSearchContainer (props) {
     doUpdateUseragentsInput,
     namesUseragentsFilteredByQuery,
     useragentsInput,
-    useragentsFilteredByInput
+    useragentsFilteredByInput,
+    queryObject
   } = props
 
+  if (useragentsInput === undefined && !queryObject.useragents) {
+    return(
+        <div className='inactive-filter'>
+        <p>by useragents: <button onClick={()=>doUpdateUseragentsInput('')}>Set...</button> </p>
+        </div>
+    )
+  }
+  if (useragentsInput === undefined && queryObject.useragents) {
+    return(
+        <div className='inactive-filter'>
+        <p>by useragents:
+        <button
+      className='f6 link dim ba b--black pv1 dib ml2 ph3 black bg-washed-blue magic-pointer'
+      onClick={()=>doUpdateUseragentsInput(queryObject.useragents)}>
+        /{queryObject.useragents}/
+        </button>
+        </p>
+        <ActiveFilter searchFilter={'useragents'} results={namesUseragentsFilteredByQuery}/>
+        </div>
+    )
+  }
+
   return (
-    <div id='test-tags-search'>
-      <h2>Filter by user-agents</h2>
+      <form id='test-tags-search'>
       <SearchBar searchFilter={'useragents'} doUpdateInput={doUpdateUseragentsInput} input={useragentsInput} />
       <SearchResults searchFilter={'useragents'} results={useragentsFilteredByInput} />
-      <ActiveFilter searchFilter={'useragents'} results={namesUseragentsFilteredByQuery}/>
-    </div>
+      </form>
   )
 }
 export default connect(
   'doUpdateUseragentsInput',
+  'selectQueryObject',
   'selectUseragentsInput',
   'selectUseragentsFilteredByInput',
   'selectNamesUseragentsFilteredByQuery',
