d681972c356981e66a24a6811be2e79c8155c1fc
Zach! Mandeville <zz@ii.coop>
2019-05-07 10:25:58 +1200

    header no underline on logo, footer no repeated logo

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -9,7 +9,6 @@ export default () => (
     <a className='contain bg-packet bg-center h-100 w4 mr4' href='https://packet.net' title='leads to external packet homepage'>
     <span className='o-0'>packet</span>
     </a>
-    <h1 className='ma0 f4 fw4 pl2 avenir'>APISnoop</h1>
     </div>
     <div id='source-code' className='flex items-center'>
     </div>
