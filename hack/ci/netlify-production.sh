f9e71617f0c584024722a1416e946bad6b66ea23
Hippie Hacker <hh@ii.coop>
2019-05-07 04:36:49 +1200

    Ensure hack/ci/CISYSTEM-TRIGGER.sh maps correctly.

---
+++ hack/ci/netlify-production.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+#set -e
+echo PRODUCTION: Merge or Commit into Master
+# https://www.netlify.com/docs/continuous-deployment/#environment-variables
+env
+
+find .
+
+curl https://storage.googleapis.com/pub/gsutil.tar.gz | tar xfz -
+export PATH=$PWD/gsutil:$PATH
+
+# We may need to spend some time looping here and wait for it to exist...
+
+# JOBID=$(gsutil cat gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/latest-build.txt)
+NEW_BUCKET="gs-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOBID/artifacts/"
+
+echo $NEW_BUCKET >> audit-sources.yaml
+cat audit-sources.yaml
+cp audit-sources.yaml app/public/audit-sources.yaml
+
+cd app
+npm install
+npm run build
+
