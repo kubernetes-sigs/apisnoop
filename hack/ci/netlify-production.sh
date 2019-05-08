fc56f3f75d46e66c605fc9230c4ee81199b7539e
Hippie Hacker <hh@ii.coop>
2019-05-09 05:48:08 +1200

    Use the latest successful job build as the data for apisnoop.cncf.io

--- hack/ci/netlify-production.sh
+++ hack/ci/netlify-production.sh
@@ -18,9 +18,12 @@ export PATH=$PWD/gsutil:$PATH
 
 # This job ID will be the lastest-build, which may come from a slightly earlier commit
 # Let's figure out a way to poll github+prow to know when this job is complete
-JOBID=$(gsutil cat gs://apisnoop/logs/apisnoop-postprocess-audits/latest-build.txt)
+# The latest build may not be done yet... and with the expiry, for now we will use
+# the latest successful build.
+# JOBID=$(gsutil cat gs://apisnoop/logs/apisnoop-postprocess-audits/latest-build.txt)
+gsutil ls 'gs://apisnoop/logs/apisnoop-postprocess-audits/*/artifacts/*/*/endpoints.json'
+JOB_ID=$(gsutil ls gsutil ls 'gs://apisnoop/logs/apisnoop-postprocess-audits/*/artifacts/*/*/endpoints.json' | sort -n | sed s:finished.json:: | tail -1 | awk -F/ '{print $6}')
 NEW_BUCKET="gs-bucket: apisnoop/logs/apisnoop-postprocess-audits/$JOBID/artifacts/"
-
 # We only need to know which bucket to pull from:
 echo $NEW_BUCKET >> audit-sources.yaml
 cat audit-sources.yaml
