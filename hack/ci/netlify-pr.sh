9b086fc1a7cefb2a926a1c6bb6f938111847743b
Hippie Hacker <hh@ii.coop>
2019-05-15 05:27:37 +1200

    Try to drop a tmate within the netlify CI

--- hack/ci/netlify-pr.sh
+++ hack/ci/netlify-pr.sh
@@ -21,9 +21,30 @@ PR_JOB_ID=$(
     | sort -n | tail -1 | awk -F/ '{print $6}')
 PR_BUCKET="gs-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOB_ID/artifacts/"
 
-git fetch git@github.com:cncf/apisnoop.git master
-git fetch git@github.com:cncf/apisnoop.git $BRANCH
-# git fetch https://github.com/cncf/apisnoop $BRANCH
+# git fetch git@github.com:cncf/apisnoop.git master
+# git fetch git@github.com:cncf/apisnoop.git $BRANCH
+# Try installing tmate
+apt-get update -y
+apt-get install tmate -y
+# Drop a tmate within the CI to debug
+TMUX_TMPDIR=$(mktemp -d /tmp/tmate-ci-XXX)
+socket=$TMUX_TMPDIR/socket
+ssh_key=$TMUX_TMPDIR/id_rsa
+ssh-keygen -f $ssh_key -t rsa -N ''
+eval $(ssh-agent)
+ssh-add $ssh_key
+tmate -S $socket \
+      new-session \
+      -d -x 80 -y 25 \
+      -s ci-session \
+      -n ci-window \
+      /bin/bash --login
+tmate -S $socket wait tmate-ready
+tmate -S $socket display -p '#{tmate_ssh} # #{tmate_web}'
+sleep 9999
+
+git fetch https://github.com/cncf/apisnoop $BRANCH
+git fetch https://github.com/cncf/apisnoop $BRANCH
 git remote -v
 git branch -av
 
