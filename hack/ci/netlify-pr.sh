38e5101fb29cbaf75dc79b2462164dbfecbed0ed
Hippie Hacker <hh@ii.coop>
2019-05-15 04:10:14 +1200

    Speed up Netlify Deploys by not generating data if we don't need to

--- hack/ci/netlify-pr.sh
+++ hack/ci/netlify-pr.sh
@@ -2,23 +2,45 @@
 set -x
 #set -e
 echo PR: PR Commit Netlify Deploy
+
 # https://www.netlify.com/docs/continuous-deployment/#environment-variables
 env
 
 curl https://storage.googleapis.com/pub/gsutil.tar.gz | tar xfz -
 export PATH=$PWD/gsutil:$PATH
 
-# We may need to spend some time looping here and wait for it to exist...
-#sleep 25m
+PRODUCTION_JOB_ID=$(
+  gsutil ls \
+         'gs://apisnoop/logs/apisnoop-postprocess-audits/*/artifacts/*/*/endpoints.json'\
+    | sort -n | tail -1 | awk -F/ '{print $6}')
+PRODUCTION_BUCKET="gs-bucket: apisnoop/logs/apisnoop-postprocess-audits/$PRODUCTION_JOB_ID/artifacts/"
 
-# JOBID=$(gsutil cat gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/latest-build.txt)
-JOB_ID=$(gsutil ls "gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/*/artifacts/*/*/endpoints.json" | sort -n | tail -1 | awk -F/ '{print $6}')
-if [ -z ${JOB_ID+x} ]
+PR_JOB_ID=$(
+  gsutil ls \
+         "gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/*/artifacts/*/*/endpoints.json" \
+    | sort -n | tail -1 | awk -F/ '{print $6}')
+PR_BUCKET="gs-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOB_ID/artifacts/"
+
+git fetch origin master
+
+# Ensure there are no changes to audit-sources and data-gen
+if git diff --quiet master -- audit-sources.yaml  data-gen/
 then
-  echo 'This initial job / site will not load well until the data has generated'
-  JOB_ID=$(gsutil ls "gs://apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/*/started.json" | sort -n | tail -1 | awk -F/ '{print $6}')
+  # If no changes, use the production
+  BUCKET_PREFIX=$PRODUCTION_BUCKET
+else
+  echo "There are changes from master, lets use the most recent changes"
+  if [ -z ${PR_JOB_ID+x} ]
+  then
+    echo "This initial PR job will need to use the master/production data"
+    echo "You'll need to trigger another commit after the data is generated"
+    BUCKET_PREFIX=$PRODUCTION_BUCKET
+  else
+    echo "Using most recent data"
+    echo "New data may be available soon, retrigger netlify build in ~30 minutes"
+    BUCKET_PREFIX=$PR_BUCKET
+  fi
 fi
-NEW_BUCKET="gs-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOB_ID/artifacts/"
 
 echo $NEW_BUCKET >> audit-sources.yaml
 cat audit-sources.yaml
--- hack/ci/prow-pr.sh
+++ hack/ci/prow-pr.sh
@@ -1,9 +1,19 @@
 #1/bin/bash
 set -e
 set -x
-export APISNOOP_DEST=$ARTIFACTS
-./apisnoop.sh --install
-./apisnoop.sh --update-cache
-./apisnoop.sh --process-cache
-find $HOME
-find /logs/artifacts
+
+git fetch origin master
+
+# Ensure there are no changes to audit-sources and data-gen
+if git diff --quiet master -- audit-sources.yaml  data-gen/
+then
+  echo "There are no changes from master"
+  echo "Not generating new data"
+else
+  echo "There are changes from master, lets use the most recent changes"
+  export APISNOOP_DEST=$ARTIFACTS
+  ./apisnoop.sh --install
+  ./apisnoop.sh --update-cache
+  ./apisnoop.sh --process-cache
+  find /logs/artifacts
+fi
