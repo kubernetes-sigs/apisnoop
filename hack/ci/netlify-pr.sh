f4e7d24640c49886a93d0f592f07638b1d3dced8
Hippie Hacker <hh@ii.coop>
2019-05-21 14:10:28 +0200

    Hack netlify on my branch

--- hack/ci/netlify-pr.sh
+++ hack/ci/netlify-pr.sh
@@ -21,34 +21,34 @@ PR_JOB_ID=$(
     | sort -n | tail -1 | awk -F/ '{print $6}')
 PR_BUCKET="gs-bucket: apisnoop/pr-logs/pull/$REVIEW_ID/apisnoop-process-audits/$JOB_ID/artifacts/"
 
-# TMATE_TMPDIR=$(mktemp -d /tmp/tmate-ci-XXX)
-# # install tmate
-# curl -L \
-#  https://github.com/tmate-io/tmate/releases/download/2.2.1/tmate-2.2.1-static-linux-amd64.tar.gz \
-#  | tar xvfzC - $TMATE_TMPDIR --strip-components 1
-# export PATH=$TMATE_TMPDIR:$PATH
-# # setup socket and ssh key
-# socket=$TMATE_TMPDIR/socket
-# ssh_key=$TMATE_TMPDIR/id_rsa
-# ssh-keygen -f $ssh_key -t rsa -N ''
-# # was ensure how to specify key to tmate
-# eval $(ssh-agent)
-# ssh-add $ssh_key
-# # launch detached tmate
-# tmate -u -S $socket \
-#   new-session \
-#  -d -x 80 -y 25 \
-#  -s ci-session \
-#  -n ci-window \
-#   /bin/bash --login
-# # wait until it is ready
-# tmate -S $socket wait tmate-ready
-# # display the tmate ssh and web connections strings
-# tmate -S $socket display -p '#{tmate_ssh} # #{tmate_web}'
+TMATE_TMPDIR=$(mktemp -d /tmp/tmate-ci-XXX)
+# install tmate
+curl -L \
+ https://github.com/tmate-io/tmate/releases/download/2.2.1/tmate-2.2.1-static-linux-amd64.tar.gz \
+ | tar xvfzC - $TMATE_TMPDIR --strip-components 1
+ export PATH=$TMATE_TMPDIR:$PATH
+ # setup socket and ssh key
+ socket=$TMATE_TMPDIR/socket
+ ssh_key=$TMATE_TMPDIR/id_rsa
+ ssh-keygen -f $ssh_key -t rsa -N ''
+ # was ensure how to specify key to tmate
+ eval $(ssh-agent)
+ ssh-add $ssh_key
+ # launch detached tmate
+tmate -u -S $socket \
+  new-session \
+ -d -x 80 -y 25 \
+ -s ci-session \
+ -n ci-window \
+  /bin/bash --login
+# wait until it is ready 
+tmate -S $socket wait tmate-ready
+# display the tmate ssh and web connections strings
+tmate -S $socket display -p '#{tmate_ssh} # #{tmate_web}'
 # # Should probably replace this sleep, with a poll mechanism
 # # probably just 'pkill sleep' for now
 # # what we really want is to pause and what for signal
-# sleep 600 || true
+sleep 600 || true
 # # five mins is enough to test and not block the CI job
 
 git remote add github https://github.com/cncf/apisnoop.git
