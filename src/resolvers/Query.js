21a7236479c8d089766c1d756dfd3741064b62dc
Zach! Mandeville <zz@ii.coop>
2019-07-01 19:38:40 +1200

    basic filter added to endpoints

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,8 +2,30 @@ function auditLogs (parent, args, context, index)  {
   return context.prisma.auditLogs();
 };
 
-function endpoints (parent, args, context, index) {
-  return context.prisma.endpoints();
+async function endpoints (parent, args, context, index) {
+  const where = args.operationID ? {
+      operationID_contains: args.operationID
+    } : {}
+
+  const endpoints = await context.prisma.endpoints({
+    where
+  });
+
+  return endpoints;
+};
+
+async function feed(parent, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where
+  })
+  return links
 }
 
 module.exports = {
