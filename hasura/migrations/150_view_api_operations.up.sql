c170c0bba9bdbb066e766a903028af44db889d7a
Zach Mandeville <zz@sharing.io>
2019-08-05 01:26:53 +0000

    api_operations has no select subqueries nor requires raw_paths
    We had an interim view for our operations called raw_paths, but I was able to
    clean up the logic for our api_operations to no longer need this.  In addition,
    we had 2 or 3 select sub-queries, which can greatly hamper performance.  Now,
    everything is done through lateral joins with no nested subqueries

--- hasura/migrations/150_view_api_operations.up.sql
+++ hasura/migrations/150_view_api_operations.up.sql
@@ -1,29 +1,29 @@
 -- api_operations view
 -- #+NAME: api_operations view
 
--- DROP VIEW api_operations CASCADE;
 CREATE OR REPLACE VIEW "public"."api_operations" AS 
-  SELECT (d.value ->> 'operationId'::text) AS operation_id,
-         uuid_generate_v1() as id,
-         raw_paths.raw_swagger_id as raw_swagger_id,
+  SELECT raw_swaggers.id AS raw_swagger_id,
+         paths.key AS path,
+         regex_from_path(paths.key) as regex,
          d.key AS method,
-         raw_paths.path as path,
-         regex_from_path(raw_paths.path) as regex,
+         (d.value ->> 'operationId'::text) AS operation_id,
          ((d.value -> 'x-kubernetes-group-version-kind'::text) ->> 'group'::text) AS k8s_group,
          ((d.value -> 'x-kubernetes-group-version-kind'::text) ->> 'version'::text) AS k8s_version,
          ((d.value -> 'x-kubernetes-group-version-kind'::text) ->> 'kind'::text) AS k8s_kind,
-         ( SELECT split_part((cat_tag.value ->> 0), '_'::text, 1) AS split_part
-             FROM jsonb_array_elements((d.value -> 'tags'::text)) cat_tag(value)) AS category,
          (d.value ->> 'description'::text) AS description,
          (d.value ->> 'x-kubernetes-action'::text) AS x_kubernetes_action,
-         ( SELECT string_agg(btrim((jsonstring.value)::text, '"'::text), ', '::text) AS string_agg
-              FROM jsonb_array_elements((d.value -> 'tags'::text)) jsonstring(value)) AS tags,
-         ( SELECT string_agg(btrim((jsonstring.value)::text, '"'::text), ', '::text) AS string_agg
-              FROM jsonb_array_elements((d.value -> 'schemes'::text)) jsonstring(value)) AS schemes,
          (d.value -> 'consumes'::text) AS consumes,
          (d.value -> 'responses'::text) AS responses,
          (d.value -> 'parameters'::text) AS parameters,
-         (lower((d.value ->> 'description'::text)) ~~ '%deprecated%'::text) AS deprecated
-    FROM (raw_paths
-          JOIN LATERAL jsonb_each(raw_paths.value) d(key, value) ON (true))
-   ORDER BY (d.value ->> 'operationId'::text);
+         (lower((d.value ->> 'description'::text)) ~~ '%deprecated%'::text) AS deprecated,
+         split_part((cat_tag.value ->> 0), '_'::text, 1) AS category,
+         string_agg(btrim((jsonstring.value)::text, '"'::text), ', '::text) AS tags,
+         string_agg(btrim((schemestring.value)::text, '"'::text), ', '::text) AS schemes
+    FROM raw_swaggers
+    , jsonb_each((raw_swaggers.data -> 'paths'::text)) paths(key, value)
+    , jsonb_each(paths.value) d(key, value)
+    , jsonb_array_elements((d.value -> 'tags'::text)) cat_tag(value)
+    , jsonb_array_elements((d.value -> 'tags'::text)) jsonstring(value)
+    , jsonb_array_elements((d.value -> 'schemes'::text)) schemestring(value)
+   GROUP BY raw_swaggers.id, paths.key, d.key, d.value, cat_tag.value
+   ORDER BY paths.key;
