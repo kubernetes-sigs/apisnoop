ce33606fd4912c9e036804999279e06a09660709
Zach Mandeville <zz@sharing.io>
2019-08-07 20:03:22 +0000

    rm docker apps that are now in frontends/

--- hasura/migrations/130_populate_swaggers.up.sql
+++
@@ -1,8 +0,0 @@
-delete from raw_swaggers;
-select * from load_swagger_via_curl('master');
--- select * from load_swagger_via_curl('release-1.15');
--- select * from load_swagger_via_curl('release-1.14');
--- select * from load_swagger_via_curl('release-1.13');
--- select * from load_swagger_via_curl('release-1.12');
--- select * from load_swagger_via_curl('release-1.11');
--- select * from load_swagger_via_curl('release-1.10');
