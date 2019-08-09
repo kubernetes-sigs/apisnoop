8242444a66b0c1736395d4498fc8b404b45bcb73
Hippie Hacker <hh@ii.coop>
2019-08-09 15:59:32 +0000

    Clean up

--- apps/hasura/migrations/130_populate_swaggers.up.sql
+++ apps/hasura/migrations/130_populate_swaggers.up.sql
@@ -1,3 +1,7 @@
+
+
+-- #+NAME: reload swaggers for particluar releases
+
 delete from raw_swaggers;
 select * from load_swagger_via_curl('master');
 -- select * from load_swagger_via_curl('release-1.15');
