25df5d2b9a937125366629a7fe584facde5a86ef
Hippie Hacker <hh@ii.coop>
2019-10-08 22:43:31 +0000

    Saving before day off
    Co-Authored-By: Zach Mandeville <zz@ii.coop>

--- apps/hasura/migrations/900_load_and_populate_swaggers.up.sql
+++ apps/hasura/migrations/900_load_and_populate_swaggers.up.sql
@@ -10,6 +10,7 @@
 delete from bucket_job_swagger;
  select * from load_bucket_job_swagger_via_curl('ci-kubernetes-e2e-gci-gce', '1173412183980118017');
  select * from load_bucket_job_swagger_via_curl('ci-kubernetes-e2e-gci-gce', '1178464478988079104');
-REFRESH MATERIALIZED VIEW api_operation_material;
+ select * from load_bucket_job_swagger_via_curl('ci-kubernetes-e2e-gci-gce', '1181584183475048448');
+ REFRESH MATERIALIZED VIEW api_operation_material;
  -- HINT:  Create a unique index with no WHERE clause on one or more columns of the materialized view^
 -- REFRESH MATERIALIZED VIEW CONCURRENTLY api_operation_parameter_material;
--- apps/hasura/migrations/910_populate_audit_events.up.sql
+++ apps/hasura/migrations/910_populate_audit_events.up.sql
@@ -5,7 +5,8 @@
 -- #+NAME: reload sample audit event 
 
 delete from raw_audit_event;
-select * from load_audit_events('ci-kubernetes-e2e-gci-gce', '1178464478988079104');
 select * from load_audit_events('ci-kubernetes-e2e-gci-gce', '1173412183980118017');
+select * from load_audit_events('ci-kubernetes-e2e-gci-gce', '1178464478988079104');
+select * from load_audit_events('ci-kubernetes-e2e-gci-gce', '1181584183475048448');
  REFRESH MATERIALIZED VIEW kind_field_path_material;
  REFRESH MATERIALIZED VIEW podspec_field_coverage_material;
