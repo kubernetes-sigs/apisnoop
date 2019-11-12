da692d03ee84a1529c578d305aa87b061c8ae31a
Caleb Woodbine <caleb@ii.coop>
2019-11-12 16:13:06 +1300

    Add: golang 1.13.4, rsync, warning for existing customize_environment

--- org/google-cloudshell/1-write-customize_environment.sh
+++ org/google-cloudshell/1-write-customize_environment.sh
@@ -1,4 +1,4 @@
-cat <<EOL > ~/.customize_environment
+cat << EOL > ~/.customize_environment
 #!/bin/bash
 
 DEVSHELL_USER=\${DEVSHELL_USER-$USER}
@@ -12,7 +12,7 @@ sudo apt-get purge -y emacs emacsen-common
 echo "deb http://londo.ganneff.de stretch main" | sudo tee /etc/apt/sources.list.d/emacs.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AD287B4E92138B93
 sudo apt-get update
-sudo apt-get install -y tmate || true
+sudo apt-get install -y tmate rsync || true
 sudo apt autoremove -y emacs*
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo su -c 'echo "deb http://ftp.debian.org/debian buster main" >> /etc/apt/sources.list.d/buster-src.list'
@@ -20,6 +20,12 @@ sudo apt update
 sudo apt install -y emacs
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo apt update
+
+sudo wget -O go.tgz "https://golang.org/dl/go1.13.4.linux-amd64.tar.gz"
+sudo su -c 'echo "692d17071736f74be04a72a06dab9cac1cd759377bd85316e52b2227604c004c  go.tgz" | sha256sum -c -' || exit 1
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go.tgz
+sudo rm go.tgz
 
 # some homefolder setup
 touch /home/\$DEVSHELL_USER/.ssh/authorized_keys
@@ -30,8 +36,7 @@ if [ ! -d /home/\$DEVSHELL_USER/.emacs.d ]; then
    su -c "git clone --recursive https://github.com/iimacs/.emacs.d /home/\$DEVSHELL_USER/.emacs.d" \$DEVSHELL_USER
    # A cache of packages that are usually required...
    # speeds up first load
-   curl https://storage.googleapis.com/apisnoop/dev/iimacs-cloudshell-cache.tgz \
-           | su -c "tar xzfC - /home/\$DEVSHELL_USER/.emacs.d" \$DEVSHELL_USER
+   curl https://storage.googleapis.com/apisnoop/dev/iimacs-cloudshell-cache.tgz | su -c "tar xzfC - /home/\$DEVSHELL_USER/.emacs.d" \$DEVSHELL_USER
    mv /home/\$DEVSHELL_USER/.emacs.d/.spacemacs-hh \
       /home/\$DEVSHELL_USER/.emacs.d/.spacemacs-alpha
 else
@@ -53,10 +58,13 @@ fi
 if [ ! -f /home/\$DEVSHELL_USER/.tmux.conf ]; then
    echo Setting up tmux
    # echo set -s escape-time 0 > su -c "tee /home/\$DEVSHELL_USER/.tmux.conf"
-   cat <<EOF | su -c "tee /home/\$DEVSHELL_USER/.tmux.conf" \$DEVSHELL_USER
+   cat << EOF | su -c "tee /home/\$DEVSHELL_USER/.tmux.conf" \$DEVSHELL_USER
         set -s escape-time 0
-EOF
+  EOF
 fi
+
+echo "unset TMUX" >> /home/\$DEVSHELL_USER/.bashrc
+
 #ls -a | grep -v .customize_environment | tail +3 | xargs -n 1 rm  -rf
 wall ".customize_environment is complete"
 EOL
--- org/google-cloudshell/README.md
+++ org/google-cloudshell/README.md
@@ -6,9 +6,14 @@
 Initializes the Google Cloudshell container with emacs.
 
 Once launched, run
-```
+```bash
 bash org/google-cloudshell/1-write-customize_environment.sh
 ```
 
+---
 
 [![Open in Cloud Shell](https://gstatic.com/cloudssh/images/open-btn.png)](https://console.cloud.google.com/cloudshell/open?git_repo=https://github.com/cncf/apisnoop&tutorial=org/google-cloudshell/README.md)
+
+---
+
+Warning: if you have an existing `.customize_environment`, this script will remove it; proceed with caution
