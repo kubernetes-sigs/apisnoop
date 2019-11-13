5153a91a974d28cf3dd1c8883d39ad209db2f360
zach <zz@ii.coop>
2019-11-13 03:53:14 +0000

    create basic frontend that pings internal hasura endpoint

---
+++ www/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import ApolloClient from 'apollo-boost';  
+  import { setClient } from 'svelte-apollo';
+  import Stats from './components/stats.svelte';
+
+	export let name;
+
+  const client = new ApolloClient({ uri: 'http://127.0.0.1:4040/v1/graphql' });
+  setClient(client);
+</script>
+
+<style>
+	h1 {
+		color: indigo;
+	}
+</style>
+
+<h1>apisnoop</h1>
+<Stats />
+
+
