639a98b777db82b6b24c04365a93b3c3c6cc51a5
zach <zz@ii.coop>
2020-01-08 22:18:08 +0000

    remove unused code from index.svelte

--- apps/webapp/src/routes/index.svelte
+++ apps/webapp/src/routes/index.svelte
@@ -1,33 +1,3 @@
-<script context="module">
-    import client from "../apollo.js";
-    import { gql } from "apollo-boost";
-    import { ALL_BUCKETS_AND_JOBS_SANS_LIVE } from '../queries';
-
-    export async function preload() {
-    return {
-        cache: await client.query({query: ALL_BUCKETS_AND_JOBS_SANS_LIVE})
-    };
-    }
-</script>
-
-<script>
- import { afterUpdate } from 'svelte';
- import { restore, query } from 'svelte-apollo';
- import {
-     rawMetadata,
-     bucketsAndJobs,
-     defaultBucketAndJob
- } from '../stores';
-
- export let cache;
-
- restore(client, ALL_BUCKETS_AND_JOBS_SANS_LIVE, cache.data);
- const metadataFromQuery = query(client, {query: ALL_BUCKETS_AND_JOBS_SANS_LIVE})
- rawMetadata.set($metadataFromQuery.data.bucket_job_swagger);
-
- afterUpdate(() => console.log({md: $rawMetadata, bj: $bucketsAndJobs, dbj: $defaultBucketAndJob}));
-</script>
-
 <svelte:head>
     <title>APISnoop</title>
 </svelte:head>
