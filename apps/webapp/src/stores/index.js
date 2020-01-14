a281c9e186a1c6dfa8827961338f65438c8df399
zach <zz@ii.coop>
2020-01-14 01:57:31 +0000

    add depth and coverage stores

--- apps/webapp/src/stores/index.js
+++ apps/webapp/src/stores/index.js
@@ -34,7 +34,7 @@ export const bucketsAndJobs = derived(rawMetadata, ($rm, set) => {
         let bj = mapValues(buckets, (allJobs) => {
             let [latestJob] = allJobs
                 .sort((a,b) => new Date(b.job_timestamp) > new Date(a.job_timestamp))
-                .map(j => j.job);
+                .map(j => ({job: j.job, timestamp: j.job_timestamp}));
 
             let jobs = allJobs.map(j => ({job: j.job, timestamp: j.job_timestamp}));
 
@@ -58,7 +58,8 @@ export const defaultBucketAndJob = derived(bucketsAndJobs, ($bj, set) => {
 
         set({
             bucket: defaultBucket,
-            job: $bj[defaultBucket].latestJob
+            job: $bj[defaultBucket].latestJob.job,
+            timestamp: $bj[defaultBucket].latestJob.timestamp
         });
     }
 });
@@ -124,6 +125,40 @@ export const sunburst = derived(groupedEndpoints, ($gep, set) => {
     } else {
         set({})
     }
+});
+
+// Based on the url params, the exact [level, category, endpoint] we are zoomed into
+export const currentDepth = writable([]);
+
+// Based on mousing over sunburst, the full path of the node we have moused over.
+export const activePath = writable([]);
+
+export const breadcrumb = derived([currentDepth, activePath],
+                                  ([$currentDepth, $activePath]) => $currentDepth.concat($activePath));
+
+export const coverageAtDepth = derived([currentDepth, endpoints], ([$cd, $eps], set) => {
+    let eps;
+    if (isEmpty($eps)) {
+        set({})
+        return;
+    } else if ($cd.length === 0) {
+        eps = $eps;
+    } else if ($cd.length === 1) {
+        eps = $eps.filter(ep => ep.level === $cd[0])
+    } else if ($cd.length === 2) {
+        eps = $eps.filter(ep => ep.level === $cd[0] && ep.category === $cd[1])
+    } else if ($cd.length === 3) {
+        eps = $eps.filter(ep => ep.level === $cd[0] && ep.category === $cd[1] && ep.operationId === $cd[2])
+    }
+
+    let totalEndpoints = eps.length;
+    let testedEndpoints = eps.filter(ep => ep.tested).length;
+    let confTestedEndpoints = eps.filter(ep => ep.conf_tested).length;
+    set({
+        totalEndpoints,
+        testedEndpoints,
+        confTestedEndpoints
+    });
 });
 
 fetchBucketsAndJobs();
