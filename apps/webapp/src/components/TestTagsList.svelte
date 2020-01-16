6f055ad100c15bd3c6d48afbbc9bc4b227259227
zach <zz@ii.coop>
2020-01-16 02:02:48 +0000

    wip: add new test tags component

---
+++ apps/webapp/src/components/TestTagsList.svelte
@@ -0,0 +1,29 @@
+<script>
+ import { testsAndTagsForEndpoint } from '../stores';
+ import { uniq } from 'lodash-es';
+ import { afterUpdate } from 'svelte'
+ $: testTags = uniq($testsAndTagsForEndpoint[0].tests.map(tt => tt.test_tag));
+ $: tests = uniq($testsAndTagsForEndpoint[0].tests.map(tt => tt.test));
+
+ afterUpdate(()=>console.log({tt: $testsAndTagsForEndpoint, testTags}));
+</script>
+
+
+{#if testTags.length > 0}
+    <div id='test-tags'>
+        <ul>
+            {#each testTags as testTag}
+                <li>{testTag}</li>
+            {/each}
+        </ul>
+    </div>
+    <div id='tests'>
+        <ul>
+            {#each tests as test}
+                <li>{test}</li>
+            {/each}
+        </ul>
+    </div>
+{/if}
+
+
