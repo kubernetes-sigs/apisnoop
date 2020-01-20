2e5f159aaafea618e7065b3b454006e434d51b77
zach <zz@ii.coop>
2020-01-20 21:59:00 +0000

    add helpers for matching on regex

--- apps/webapp/src/lib/helpers.js
+++ apps/webapp/src/lib/helpers.js
@@ -1,7 +1,9 @@
 import {
     forEach,
     trimEnd,
-    trimStart
+    trimStart,
+    flatten,
+    uniq
 } from 'lodash-es';
 
 export const updateQueryParams = (page, query) => {
@@ -20,4 +22,23 @@ export const updateQueryParams = (page, query) => {
     let queryNames = Object.keys(queryStrings);
     let fullQueryString = trimEnd(queryNames.reduce((acc, cur) => `${acc}${queryStrings[cur]}`, '?'), '&');
     return fullQueryString;
+};
+
+
+export const isValidRegex = (regex) => {
+    try {
+        new RegExp(regex);
+    } catch (e) {
+        return false;
+    }
+    return true;
+};
+
+export const hitByMatchingUseragent = (useragents, regex, endpoint) => {
+    // return true if endpoint is hit by any useragent
+    // in array of useragents, filtered by a regex match.
+    regex = new RegExp(regex);
+    let matchingUseragents = useragents.filter(ua => regex.test(ua.useragent));
+    let endpointsHitByUseragents = uniq(flatten(matchingUseragents.map(ua => ua.operation_ids)));
+    return endpointsHitByUseragents.includes(endpoint.operation_id);
 };
