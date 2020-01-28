58db59ee2ee9088bfb218dda0b2f8454a0159ec8
Hippie Hacker <hh@ii.coop>
2020-01-29 12:25:09 +1300

    Simplify Ingress
    Co-Authored-By: Zach Mandeville <zz@ii.coop>

--- apps/webapp/src/apollo.js
+++ apps/webapp/src/apollo.js
@@ -2,5 +2,6 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'node-fetch';
 
 const GQL_ENDPOINT = 'http://hasura:8080/v1/graphql'
+// const GQL_ENDPOINT = 'http://localhost/hasura/v1/graphql';
 
 export default new ApolloClient({ uri: GQL_ENDPOINT, fetch: fetch });
