ec4a1a5b7e43671bbd9cce38dd4d81d9a85abd6f
Hippie Hacker <hh@ii.coop>
2020-01-31 05:52:17 +1300

    Add simple setup.sh to bring up local env

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
-cd ~/cncf/apisnoop/
+# TODO Create a K8s cluster Running Kubemacs
 
-kind delete cluster --name kind
+# [[file:~/cncf/apisnoop/deployment/k8s/kind-cluster-config.yaml::#%20kind-cluster-config.yaml][kind-cluster-config.yaml (enabling Dynamic Audit Logging)]]
+
+
+wget https://raw.githubusercontent.com/cncf/apisnoop/master/deployment/k8s/kind-cluster-config.yaml
 kind create cluster --name kind \
      --image kindest/node:v1.17.0@sha256:9512edae126da271b66b990b6fff768fbb7cd786c7d39e86bdf55906352fdf62 \
-     --config apisnoop/deployment/k8s/kind-cluster-config.yaml
+     --config kind-cluster-config.yaml
+# these two commands just ensure our image is cached and available quickly
+docker pull gcr.io/apisnoop/kubemacs:0.9.23
 kind load docker-image --nodes kind-worker gcr.io/apisnoop/kubemacs:0.9.23
-kubectl apply -f apisnoop/deployment/k8s/kubemacs.yaml
-kubectl apply -f apisnoop/deployment/k8s/kubemacs.yaml
+kubectl create namespace kubemacs
+kubectl apply -f https://raw.githubusercontent.com/cncf/apisnoop/master/deployment/k8s/kubemacs.yaml
+# Careful as this will restart the dev/code writing pod
+kubectl -n kubemacs set env deployment/kubemacs \
+        GIT_COMMITTER_EMAIL=hh@ii.coop \
+        GIT_COMMITTER_NAME="Hippie Hacker" \
+        GIT_AUTHOR_EMAIL=hh@ii.coop \
+        GIT_AUTHOR_NAME="Hippie Hacker"
+
+PODNAME=$(kubectl -n kubemacs get pod --selector=app=kubemacs -o name  | sed s:pod/::)
+kubectl describe -n kubemacs pod/$PODNAME
+kubectl get -n kubemacs pod/$PODNAME
