cdc0b498ea9502e5ec2e3855e8c09f706b7cb88b
Zach Mandeville <zz@ii.coop>
2020-02-03 14:58:20 +1300

    revert audit_event to no longer be materialized

--- apps/hasura/920_populate_audit_events.up.sql
+++
@@ -1,8 +0,0 @@
--- 920: Populate Audits Up
---   :PROPERTIES:
---   :header-args:sql-mode+: :tangle ../apps/hasura/migrations/920_populate_audit_events.up.sql
---   :END:
-
-select * from load_audit_events();
-REFRESH MATERIALIZED VIEW api_operation_material;
-REFRESH MATERIALIZED VIEW api_operation_parameter_material;
