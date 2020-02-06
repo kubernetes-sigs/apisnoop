ecd67a329310f068071e5a51880c4b4757783ded
Hippie Hacker <hh@ii.coop>
2020-02-07 09:18:25 +1300

    Update local test writing env

--- deployment/k8s/local/kubemacs/setup.sh
+++ deployment/k8s/local/kubemacs/setup.sh
@@ -3,18 +3,18 @@
 if [ ! -z "$DEBUG" ]; then
     set -x
 fi
-export KUBEMACS_IMAGE="${KUBEMACS_IMAGE:-gcr.io/apisnoop/kubemacs:0.9.30}"
+export KUBEMACS_IMAGE="${KUBEMACS_IMAGE:-gcr.io/apisnoop/kubemacs:0.9.32}"
 export KUSTOMIZE_PATH="${KUSTOMIZE_PATH:-https://github.com/cncf/apisnoop/deployment/k8s/local}"
 export KIND_IMAGE="${KIND_IMAGE:- kindest/node:v1.17.0@sha256:9512edae126da271b66b990b6fff768fbb7cd786c7d39e86bdf55906352fdf62}"
 export DEFAULT_NS="${DEFAULT_NS:-ii}"
 
 docker pull $KIND_IMAGE
 
-if read -p "Press enter to destroy your current kind cluster, ^C to abort "; then
-    kind delete cluster
-fi
-curl https://raw.githubusercontent.com/cncf/apisnoop/master/deployment/k8s/kind-cluster-config.yaml -o kind-cluster-config.yaml
-kind create cluster --config kind-cluster-config.yaml --image $KIND_IMAGE
+# if read -p "Press enter to destroy your current kind cluster, ^C to abort "; then
+#     kind delete cluster
+# fi
+# curl https://raw.githubusercontent.com/cncf/apisnoop/master/deployment/k8s/kind-cluster-config.yaml -o kind-cluster-config.yaml
+# kind create cluster --config kind-cluster-config.yaml --image $KIND_IMAGE
 
 # Cache Kubemacs image
 
@@ -32,12 +32,11 @@ kind load docker-image --nodes kind-worker $KUBEMACS_IMAGE # docker->kind
 
 kubectl create ns $DEFAULT_NS
 kubectl config set-context $(kubectl config current-context) --namespace=$DEFAULT_NS
-
-#kubectl apply -k https://github.com/cncf/apisnoop/deployment/k8s/xip.io/kubemacs
 kubectl apply -k "$KUSTOMIZE_PATH/kubemacs"
 echo "Waiting for Kubemacs StatefulSet to have 1 ready Replica..."
 while [ "$(kubectl get statefulset kubemacs -o json | jq .status.readyReplicas)" != 1 ]; do
   sleep 1s
 done
 kubectl wait --for=condition=Ready pod/kubemacs-0
-kubectl exec -t -i kubemacs-0 -- attach
+echo Run the following:
+echo kubectl exec -ti kubemacs-0 -- attach
