f91b5b0f4f457c6fde028a1893dcefcfacd42baf
Zach! <webmaster@coolguy.website>
2020-02-07 09:41:17 +1300

    breadcrumb component uses improved breadcrumb store

--- apps/webapp/src/components/Breadcrumb.svelte
+++ apps/webapp/src/components/Breadcrumb.svelte
@@ -1,22 +1,25 @@
 <script>
  import { afterUpdate } from 'svelte';
- import { breadcrumb, opIDs } from '../stores';
+ import { breadcrumb, mouseOverPath, opIDs } from '../stores';
  import { levelColours, categoryColours, endpointColour } from '../lib/colours.js';
 
- $: [level , category , endpoint] = $breadcrumb;
+ $: [level, category, operation_id] = $breadcrumb;
  $: lColour = levelColours[level] || 'white';
  $: cColour = categoryColours[category] || 'white';
- $: eColour = $opIDs[endpoint]
-            ? endpointColour($opIDs[endpoint]) 
+ $: eColour = $opIDs[operation_id]
+            ? endpointColour($opIDs[operation_id]) 
             : 'white';
- $: eText = $opIDs[endpoint]
-          ? $opIDs[endpoint]['tested'] ? endpointColour($opIDs[endpoint]) : 'gray'
+ $: eTextColour = $opIDs[operation_id]
+          ? $opIDs[operation_id]['tested'] ? endpointColour($opIDs[operation_id]) : 'gray'
           : 'white';
 
+ afterUpdate(() => console.log('crumbs', $breadcrumb));
 </script>
 
 <div id='breadcrumb'>
-    <p><span style='border-color: {lColour}; background-color: {lColour};'>{level}</span><span style='background-Color: {cColour}; border-color: {cColour};'>{category}</span><span style='border-color: {eColour}; color: {eText};'>{endpoint}</span></p>
+  {#if $mouseOverPath.length > 0}
+  <p>{#if level}<span style='border-color: {lColour}; background-color: {lColour};'>{level}</span>{/if}{#if category}<span style='background-Color: {cColour}; border-color: {cColour};'>{category}</span>{/if}{#if operation_id}<span style='border-color: {eColour}; color: {eTextColour};'> {operation_id}</span>{/if}</p>
+  {/if}
 </div>
 
 <style>
