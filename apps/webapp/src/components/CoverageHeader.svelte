f4d8404755fc05f3f0a53c2f02a28d36f24ba8ff
Zach! <webmaster@coolguy.website>
2020-02-07 09:39:54 +1300

    header takes froma activeBucketAndJob to remove redundant store

--- apps/webapp/src/components/CoverageHeader.svelte
+++ apps/webapp/src/components/CoverageHeader.svelte
@@ -1,11 +1,11 @@
 <script>
  import dayjs from 'dayjs';
- import { bucketAndJobMetadata as metadata} from '../stores';
+ import { activeBucketAndJob } from '../stores';
  import { onMount, afterUpdate } from 'svelte';
 
- $: bucket = $metadata.bucket;
- $: job = $metadata.job;
- $: timestamp = dayjs($metadata.timestamp).format('DD MMMM, YYYY');
+ $: bucket = $activeBucketAndJob.bucket;
+ $: job = $activeBucketAndJob.job;
+ $: timestamp = dayjs($activeBucketAndJob.timestamp).format('DD MMMM, YYYY');
 </script>
 
 <header>
