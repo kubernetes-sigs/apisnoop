a4a8047da9260986f5d4eec50c1133edaba8d934
Hippie Hacker <hh@ii.coop>
2020-02-07 09:43:00 +1300

    Ensure single command to load working testwriting.localho.st

--- deployment/k8s/local/kubemacs/setup.sh
+++ deployment/k8s/local/kubemacs/setup.sh
@@ -1,4 +1,5 @@
-# Deploy kind
+# Save to ./setup.sh
+#   #+name: load kind
 
 if [ ! -z "$DEBUG" ]; then
     set -x
@@ -7,29 +8,21 @@ export KUBEMACS_IMAGE="${KUBEMACS_IMAGE:-gcr.io/apisnoop/kubemacs:0.9.32}"
 export KUSTOMIZE_PATH="${KUSTOMIZE_PATH:-https://github.com/cncf/apisnoop/deployment/k8s/local}"
 export KIND_IMAGE="${KIND_IMAGE:- kindest/node:v1.17.0@sha256:9512edae126da271b66b990b6fff768fbb7cd786c7d39e86bdf55906352fdf62}"
 export DEFAULT_NS="${DEFAULT_NS:-ii}"
-
+if read -p "Press enter to destroy your current kind cluster, ^C to abort "; then
+    kind delete cluster
+fi
 docker pull $KIND_IMAGE
-
-# if read -p "Press enter to destroy your current kind cluster, ^C to abort "; then
-#     kind delete cluster
-# fi
-# curl https://raw.githubusercontent.com/cncf/apisnoop/master/deployment/k8s/kind-cluster-config.yaml -o kind-cluster-config.yaml
-# kind create cluster --config kind-cluster-config.yaml --image $KIND_IMAGE
-
-# Cache Kubemacs image
-
-# ensure large images are cached
+docker pull $KUBEMACS_IMAGE # cache into docker socket
 kubectl apply --dry-run -k "$KUSTOMIZE_PATH" -o yaml \
   | grep image: | sed 's/.*:\ \(.*\)/\1/' | sort | uniq \
   | xargs -n 1 docker pull
+curl https://raw.githubusercontent.com/cncf/apisnoop/master/deployment/k8s/kind-cluster-config.yaml -o kind-cluster-config.yaml
+kind create cluster --config kind-cluster-config.yaml --image $KIND_IMAGE
+# docker->kind
+kind load docker-image --nodes kind-worker $KUBEMACS_IMAGE 
 kubectl apply --dry-run -k "$KUSTOMIZE_PATH" -o yaml \
   | grep image: | sed 's/.*:\ \(.*\)/\1/' | sort | uniq \
   | xargs -n 1 kind load docker-image
-docker pull $KUBEMACS_IMAGE # cache into docker socket
-kind load docker-image --nodes kind-worker $KUBEMACS_IMAGE # docker->kind
-
-# Deploy apisnoop services
-
 kubectl create ns $DEFAULT_NS
 kubectl config set-context $(kubectl config current-context) --namespace=$DEFAULT_NS
 kubectl apply -k "$KUSTOMIZE_PATH/kubemacs"
