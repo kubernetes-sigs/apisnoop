7c78b33e79ee7bf77f7a8379ed3d8b5d95b51258
Hippie Hacker <hh@ii.coop>
2020-02-07 15:41:04 +1300

    Tangle readme into local-setup.sh

--- deployment/k8s/local/kubemacs/local-setup.sh
+++ deployment/k8s/local/kubemacs/local-setup.sh
@@ -1,3 +1,6 @@
+# Footnotes
+
+# Run with EMAIL=me@my.net NAME="First Last" bash local-setup.sh
 NAME=${NAME:-"Hippie Hacker"}
 EMAIL=${EMAIL:-"hh@ii.coop"}
 KIND_IMAGE="kindest/node:v1.17.0@sha256:9512edae126da271b66b990b6fff768fbb7cd786c7d39e86bdf55906352fdf62"
@@ -65,48 +68,11 @@ configMapGenerator:
   - INIT_DEFAULT_DIR=/home/ii/apisnoop/deployment/k8s/local/
   - INIT_ORG_FILE=/home/ii/apisnoop/deployment/k8s/local/tilt.org
 EOF
-
-# Configuration Vars
-#   #+NAME: customization-vars
-
-NAME=${NAME:-"Hippie Hacker"}
-EMAIL=${EMAIL:-"hh@ii.coop"}
-KIND_IMAGE="kindest/node:v1.17.0@sha256:9512edae126da271b66b990b6fff768fbb7cd786c7d39e86bdf55906352fdf62"
-KIND_CONFIG="kind-cluster.yaml"
-K8S_RESOURCES="k8s-resources.yaml"
-DEFAULT_NS="ii"
-
-# kustomization.yaml
-
-#   #+NAME: kustomization-yaml
-
-cat <<EOF > kustomization.yaml
-apiVersion: kustomize.config.k8s.io/v1beta1
-kind: Kustomization
-bases:
-  - https://github.com/cncf/apisnoop/deployment/k8s/local/kubemacs
-configMapGenerator:
-- name: kubemacs-configuration
-  behavior: merge
-  literals:
-  - TZ=Pacific/Auckland
-  - GIT_NAME=${NAME}
-  - GIT_EMAIL=${EMAIL}
-  - INIT_DEFAULT_REPO=https://github.com/cncf/apisnoop.git
-  - INIT_DEFAULT_DIR=/home/ii/apisnoop/deployment/k8s/local/
-  - INIT_ORG_FILE=/home/ii/apisnoop/deployment/k8s/local/tilt.org
-EOF
-
-# Create and kind cluster and cache/load images
-
 kind create cluster --config $KIND_CONFIG --image $KIND_IMAGE
 kubectl kustomize . > $K8S_RESOURCES # uses kustomization.yaml
 K8S_IMAGES=$(cat $K8S_RESOURCES | grep image: | sed 's/.*:\ \(.*\)/\1/' | sort | uniq )
 echo $K8S_IMAGES | xargs -n 1 docker pull # cache images in docker
 echo $K8S_IMAGES | xargs -n 1 kind load docker-image --nodes kind-worker # load cache into kind-worker
-
-# Deploy and attach to the kubemacs pod
-
 kubectl create ns $DEFAULT_NS
 kubectl config set-context $(kubectl config current-context) --namespace=$DEFAULT_NS
 kubectl apply -f $K8S_RESOURCES
@@ -115,5 +81,5 @@ while [ "$(kubectl get statefulset kubemacs -o json | jq .status.readyReplicas)"
   sleep 1s
 done
 kubectl wait --for=condition=Ready pod/kubemacs-0
-echo Run the following in iTerm2, xterm, or other OSC52 Terminal:
-echo kubectl exec -ti kubemacs-0 -- attach
+kubectl wait --for=condition=Ready pod/kubemacs-0
+kubectl exec -ti kubemacs-0 -- attach
