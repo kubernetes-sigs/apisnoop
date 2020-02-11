b14ba0e353b4b2fc142f0d60dd15866437f7d77f
Hippie Hacker <hh@ii.coop>
2020-02-11 18:42:29 +1300

    move apps/kubemacs  to a submodule

--- apps/kubemacs/bin/osc52-tmate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# xtermcontrol --title k8s@iimacs.org
-tmate display -p "#{tmate_ssh} # $(date) #{client_tty}@#{host}" | osc52.sh
--- apps/kubemacs/homedir/.bash_profile
+++
@@ -1,3 +0,0 @@
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
-fi
