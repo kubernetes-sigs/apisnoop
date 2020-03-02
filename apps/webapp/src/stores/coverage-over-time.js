184464fdac71532cbca9345893358d6ed4924ead
Zach Mandeville <zz@ii.coop>
2020-03-03 10:13:05 +1300

    ensure dates are sorted for better display

--- apps/webapp/src/stores/coverage-over-time.js
+++ apps/webapp/src/stores/coverage-over-time.js
@@ -1,11 +1,16 @@
 import dayjs from 'dayjs';
 import { derived }  from 'svelte/store';
-import { sortBy } from 'lodash-es';
+import {
+    findKey,
+    flatten,
+    sortBy
+} from 'lodash-es';
 import {
     bucketsAndJobs,
     defaultBucketAndJob,
     stableEndpointStats
 } from './index.js';
+
 
 export const dates = derived(
     [bucketsAndJobs, defaultBucketAndJob],
@@ -13,26 +18,37 @@ export const dates = derived(
         if ($bjs.length === 0) {
             set([])
         } else {
-            let bucket = $default.bucket;
-            let jobs = sortBy($bjs[bucket]['jobs'], 'timestamp');
-            set(jobs.map(job => job.timestamp));
+            // let bucket = $default.bucket;
+            let buckets = Object.keys($bjs);
+            let allJobs = buckets.reduce((acc, cur) => {
+                let jobs = sortBy($bjs[cur]['jobs'], 'timestamp');
+                return [...acc, ...jobs];
+            }, []);
+
+            allJobs = sortBy(allJobs,'timestamp');
+            set(allJobs.map(job => job.timestamp));
         }
     }
 );
 
 export const coverage = derived(
-    [defaultBucketAndJob, stableEndpointStats],
-    ([$default, $stats], set) => {
-        console.log({default: $default})
+    [defaultBucketAndJob, stableEndpointStats, bucketsAndJobs],
+    ([$default, $stats, $bjs], set) => {
         if ($default.bucket === '') {
             set([]);
         } else {
-            console.log({stats: $stats })
             let coverageStats = $stats
                 .filter(stat => stat.job !== 'live')
-                .map(stat => ({...stat, timestamp: dayjs(stat.date)}));
-
-            console.log({coverageStats})
+                .map(stat => {
+                    let bucket = findKey($bjs, (o) => o.jobs.map(job => job.job).includes(stat.job))
+                    console.log({bucket});
+                    return {
+                        ...stat,
+                        bucket,
+                        timestamp: dayjs(stat.date)
+                    };
+                });
+            coverageStats = sortBy(coverageStats, 'timestamp');
             set(coverageStats);
         }
     }
