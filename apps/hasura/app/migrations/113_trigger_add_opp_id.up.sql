356121efd7739e1e20bb53a1691c428b063b7872
Zach Mandeville <zz@ii.coop>
2020-03-20 12:09:51 +1300

    make explicit the spec we are basing test writing on

--- apps/hasura/app/migrations/113_trigger_add_opp_id.up.sql
+++ apps/hasura/app/migrations/113_trigger_add_opp_id.up.sql
@@ -1,2 +1,5 @@
-CREATE TRIGGER add_op_id BEFORE INSERT ON audit_event
-  FOR EACH ROW EXECUTE PROCEDURE add_op_id();
+CREATE TRIGGER add_op_id
+  BEFORE INSERT ON audit_event
+  FOR EACH ROW
+    WHEN (NEW.job = 'live')
+    EXECUTE PROCEDURE add_op_id();
