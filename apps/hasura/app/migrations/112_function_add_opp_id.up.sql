07b176edee224f1d1217c72d7a134015352a2c34
Zach Mandeville <zz@ii.coop>
2020-03-20 12:09:51 +1300

    make explicit the spec we are basing test writing on

--- apps/hasura/app/migrations/112_function_add_opp_id.up.sql
+++ apps/hasura/app/migrations/112_function_add_opp_id.up.sql
@@ -2,8 +2,12 @@ set role dba;
 CREATE OR REPLACE FUNCTION add_op_id() RETURNS TRIGGER as $$
    import json
    from snoopUtils import load_openapi_spec, find_operation_id
+   # We want the openapis spec for the tagged image of k8s used by kind.
+   CURRENT_K8S_TAG = "v1.17.0"
+   K8S_GITHUB_RAW= "https://raw.githubusercontent.com/kubernetes/kubernetes/"
+   CURRENT_SWAGGER_URL = K8S_GITHUB_RAW + CURRENT_K8S_TAG + "/api/openapi-spec/swagger.json"
    if "spec" not in GD:
-       GD["spec"] = load_openapi_spec('https://raw.githubusercontent.com/kubernetes/kubernetes/7d13dfe3c34f44/api/openapi-spec/swagger.json')
+       GD["spec"] = load_openapi_spec(CURRENT_SWAGGER_URL)
    spec = GD["spec"]
    event = json.loads(TD["new"]["data"])
    if TD["new"]["operation_id"] is None:
