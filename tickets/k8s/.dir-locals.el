d19493097498e54712c3264e992ecd37a7c38d22
Zach Mandeville <zz@ii.coop>
2020-03-30 09:31:08 +1300

    insert function uses mock template instead of new ticket-template
    There is no readability gain between the two names and mock-template has a nice
    git history to it, that should be retained.

--- tickets/k8s/.dir-locals.el
+++ tickets/k8s/.dir-locals.el
@@ -1,12 +1,12 @@
 ((nil .
       ((eval .
-             (defun apisnoop/insert-ticket-template ()
-               "Inserts contents of current directory's ticket-template.org file into current buffer."
+             (defun apisnoop/insert-mock-template ()
+               "Inserts contents of current directory's mock-template.org file into current buffer."
                (interactive)
-               (let ((ticket-template "./ticket-template.org"))
-                 (if (file-exists-p ticket-template)
-                     (insert-file-contents ticket-template)
-                   (message "No file named %s found in current directory" ticket-template)))))
+               (let ((mock-template "./mock-template.org"))
+                 (if (file-exists-p mock-template)
+                     (insert-file-contents mock-template)
+                   (message "No file named %s found in current directory" mock-template)))))
        (eval .
              (defun apisnoop/delete-live-events ()
                "deletes all live events from audit_events table using psql.  Requires you to have already
