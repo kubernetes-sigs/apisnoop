c31a59473fdf421d16cbd881b1f37fa35c4889dc
Zach Mandeville <zz@ii.coop>
2020-03-29 21:34:41 +1300

    add apisnoop helper functions to k8s tickets directory

---
+++ tickets/k8s/.dir-locals.el
@@ -0,0 +1,16 @@
+((nil .
+      ((eval .
+             (defun apisnoop/insert-ticket-template-plz ()
+               "Inserts contents of current directory's ticket-template.org file into current buffer."
+               (interactive)
+               (let ((ticket-template "./ticket-template.org"))
+                 (if (file-exists-p ticket-template)
+                     (insert-file-contents ticket-template)
+                   (message "No file named %s found in current directory" ticket-template)))))
+       (eval .
+             (defun apisnoop/delete-live-events ()
+               "deletes all live events from audit_events table using psql.  Requires you to have already
+                started up apisnoop."
+               (interactive)
+               (shell-command "psql -c \"DELETE FROM audit_event WHERE bucket='apisnoop' and job='live'; \"")))
+       )))
