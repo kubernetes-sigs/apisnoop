5c8b27b48c1f1930bf90999e9ede89f8d8c1612d
Zach Mandeville <zz@ii.coop>
2020-04-01 16:31:55 +1300

    src comes from index.org for literate webapp

--- apps/webapp/app/src/routes/index.json.js
+++ apps/webapp/app/src/routes/index.json.js
@@ -1,17 +1,23 @@
 import client from "../apollo.js";
 import { determineBucketAndJob } from '../lib/helpers.js';
-import { ALL_BUCKETS_AND_JOBS_SANS_LIVE, STABLE_ENDPOINT_STATS } from '../queries';
+import {
+  ALL_BUCKETS_AND_JOBS_SANS_LIVE,
+  STABLE_ENDPOINT_STATS } from '../queries';
 
 export async function get(req, res) {
-    let bucketAndJobsQuery = await client.query({query: ALL_BUCKETS_AND_JOBS_SANS_LIVE});
-    let statsQuery = await client.query({query: STABLE_ENDPOINT_STATS});
-    let rawBucketsAndJobsPayload = bucketAndJobsQuery.data.bucket_job_swagger;
-    let stableEndpointStatsPayload = statsQuery.data.stable_endpoint_stats;
-    let statsAndBucketsAndJob = {stableEndpointStatsPayload, rawBucketsAndJobsPayload}
-    const payload = JSON.stringify(statsAndBucketsAndJob);
+  let bucketAndJobsQuery = await client.query({query: ALL_BUCKETS_AND_JOBS_SANS_LIVE});
+  let rawBucketsAndJobsPayload = bucketAndJobsQuery.data.bucket_job_swagger;
 
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    res.end(payload);
+  let statsQuery = await client.query({query: STABLE_ENDPOINT_STATS});
+  let stableEndpointStatsPayload = statsQuery.data.stable_endpoint_stats;
+
+  const payload = JSON.stringify({
+    stableEndpointStatsPayload,
+    rawBucketsAndJobsPayload
+  });
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(payload);
 }
--- apps/webapp/app/src/routes/index.svelte
+++ apps/webapp/app/src/routes/index.svelte
@@ -1,41 +1,31 @@
 <script context="module">
  export function preload({ params, query }) {
-	 return this.fetch(`index.json`).then(r => r.json()).then(payload => {
-		 return { payload };
-	 });
+   return this.fetch(`index.json`).then(r => r.json()).then(payload => {
+     return { payload };
+   });
  }
 </script>
 
 <script>
- import CoverageOverTime from '../components/CoverageOverTime.svelte';
+ import CoverageOverTime from '../components/CoverageOverTime.svelte'; 
  import { isEqual} from 'lodash-es';
- import { goto } from '@sapper/app';
  export let payload;
  import {
-     stableEndpointStats,
-     rawBucketsAndJobs,
-   } from '../stores';
+   stableEndpointStats,
+   rawBucketsAndJobs,
+ } from '../stores';
 
  rawBucketsAndJobs.update(raw => isEqual(raw, payload.rawBucketsAndJobsPayload)
-                                ? raw
-                                : payload.rawBucketsAndJobsPayload);
+                               ? raw
+                               : payload.rawBucketsAndJobsPayload);
 
  stableEndpointStats.update(stats => isEqual(stats, payload.stableEndpointStatsPayload)
-                                ? stats
-                                : payload.stableEndpointStatsPayload);
+                                   ? stats
+                                   : payload.stableEndpointStatsPayload);
 </script>
 
 <svelte:head>
-    <title>APISnoop</title>
+  <title>APISnoop</title>
 </svelte:head>
 <CoverageOverTime />
-<style>
- ul {
-	 margin: 0 0 1em 0;
-	 line-height: 1.5;
- }
- section#stats {
-   height: 100vh;
- }
-</style>
-
+<h1>You got a webpage</h1>
