fc554b3c1d1b08488ac430c50baaab59819f56f2
Zach Mandeville <zz@ii.coop>
2020-04-01 16:55:04 +1300

    add sunburst to homepage, rough draft

--- apps/webapp/app/src/routes/index.json.js
+++ apps/webapp/app/src/routes/index.json.js
@@ -2,6 +2,7 @@ import client from "../apollo.js";
 import { determineBucketAndJob } from '../lib/helpers.js';
 import {
   ALL_BUCKETS_AND_JOBS_SANS_LIVE,
+  ENDPOINTS_TESTS_AND_USERAGENTS,
   STABLE_ENDPOINT_STATS } from '../queries';
 
 export async function get(req, res) {
@@ -11,9 +12,18 @@ export async function get(req, res) {
   let statsQuery = await client.query({query: STABLE_ENDPOINT_STATS});
   let stableEndpointStatsPayload = statsQuery.data.stable_endpoint_stats;
 
+  let {bucket, job} = determineBucketAndJob(rawBucketsAndJobsPayload);
+
+  let endpointsTestsAndUseragentsQuery = await client.query(
+    {query: ENDPOINTS_TESTS_AND_USERAGENTS,
+     variables: {bucket, job}
+    });
+  let endpointsTestsAndUseragentsPayload = endpointsTestsAndUseragentsQuery.data;
+
   const payload = JSON.stringify({
     stableEndpointStatsPayload,
-    rawBucketsAndJobsPayload
+    rawBucketsAndJobsPayload,
+    endpointsTestsAndUseragentsPayload
   });
 
   res.writeHead(200, {
--- apps/webapp/app/src/routes/index.svelte
+++ apps/webapp/app/src/routes/index.svelte
@@ -8,24 +8,37 @@
 
 <script>
  import CoverageOverTime from '../components/CoverageOverTime.svelte'; 
+ import Sunburst from '../components/Sunburst.svelte';
  import { isEqual} from 'lodash-es';
- export let payload;
  import {
    stableEndpointStats,
    rawBucketsAndJobs,
+   endpointsTestsAndUseragents
  } from '../stores';
 
- rawBucketsAndJobs.update(raw => isEqual(raw, payload.rawBucketsAndJobsPayload)
-                               ? raw
-                               : payload.rawBucketsAndJobsPayload);
+ export let payload;
+ const {
+   rawBucketsAndJobsPayload,
+   stableEndpointStatsPayload,
+   endpointsTestsAndUseragentsPayload
+ } = payload;
 
- stableEndpointStats.update(stats => isEqual(stats, payload.stableEndpointStatsPayload)
+ rawBucketsAndJobs.update(raw => isEqual(raw, rawBucketsAndJobsPayload)
+                               ? raw
+                               : rawBucketsAndJobsPayload);
+
+ stableEndpointStats.update(stats => isEqual(stats, stableEndpointStatsPayload)
                                    ? stats
-                                   : payload.stableEndpointStatsPayload);
+                                   : stableEndpointStatsPayload);
+
+ endpointsTestsAndUseragents.update(etu => isEqual(etu, endpointsTestsAndUseragentsPayload)
+                                         ? etu
+                                         : endpointsTestsAndUseragentsPayload);
 </script>
 
 <svelte:head>
   <title>APISnoop</title>
 </svelte:head>
 <CoverageOverTime />
+<Sunburst />
 <h1>You got a webpage</h1>
