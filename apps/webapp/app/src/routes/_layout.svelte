8259a5588473f0407bfedd8aa125c6ade4f9406e
Zach Mandeville <zz@ii.coop>
2020-04-02 10:42:41 +1300

    add layout back

---
+++ apps/webapp/app/src/routes/_layout.svelte
@@ -0,0 +1,20 @@
+<script>
+ import Nav from '../components/Nav.svelte';
+
+</script>
+
+<style>
+ main {
+   position: relative;
+   max-width: 100%;
+   background-color: white;
+   padding: 2em;
+   box-sizing: border-box;
+ }
+</style>
+
+<Nav />
+
+<main>
+  <slot></slot>
+</main>
