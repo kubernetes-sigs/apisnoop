0dd3e7012ffb454af0b9c0e38e608e18c5bad9b7
Zach Mandeville <zz@ii.coop>
2020-04-01 16:29:45 +1300

    wip: explore using dynamic index page

---
+++ apps/webapp/app/src/routes/[...index].svelte
@@ -0,0 +1,20 @@
+<script context="module">
+ import { join } from 'lodash-es';
+ export function preload ({ params, query }) {
+   let path = join(params.coverage, '/');
+   return this
+     .fetch(`/${path}.json`)
+     .then(r => r.json())
+     .then(payload => ({ payload }));
+ };
+</script>
+
+<script>
+ import { onMount, afterUpdate } from 'svelte';
+ export let payload;
+
+ onMount(() => console.log('mount', payload));
+ afterUpdate(() => console.log('mount', payload));
+</script>
+
+<h1>Web Page is Getting There</h1>
