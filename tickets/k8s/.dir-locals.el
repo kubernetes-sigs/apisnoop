0be18658841e604ca731c5dc07de5c2d32705ceb
Zach Mandeville <zz@ii.coop>
2020-04-22 20:48:16 +1200

    find and format numeric comparisons

--- tickets/k8s/.dir-locals.el
+++ tickets/k8s/.dir-locals.el
@@ -38,15 +38,18 @@
                   (defun apisnoop/get-mock-test-value ()
                     "grabs value from code block title 'Mock Test In Go"
                     (save-excursion
-                      (let((mock-test (car (org-element-map (org-element-parse-buffer) 'src-block
+                      (let* ((mock-test (car (org-element-map (org-element-parse-buffer) 'src-block
                                              (lambda (src-block) 
                                                (let ((name (org-element-property :name src-block))
                                                      (value (org-element-property :value src-block)))
                                                  (if (and (stringp name) (string= name "Mock Test In Go"))
-                                                     value)))))))
-                        mock-test))))
+                                                     value))))))
+                             (separator "// TEST \\(BEGINS\\|ENDS\\) HERE")
+                           (good-stuff (cadr (s-split separator  mock-test))))
+                        good-stuff))))
             (eval .
                   (defun apisnoop/format-nil-errors (mock-test)
+                    "Formats general purpose errors (when err != null) to fit within ginkgo framework"
                     (let* ((match " *if err != nil {\n *fmt.Println(err, \"[a-z A-Z]*\")\n *return\n *}")
                            (g-pre (s-concat (s-repeat 7 " ") "framework.ExpectNoError\(")))
                       (replace-regexp-in-string match
@@ -57,6 +60,34 @@
                                                       g-err)))
                                                 mock-test))))
             (eval .
+                  (defun apisnoop/format-eq-errors (mock-test)
+                    "Formats equality comparison errors (podA != podB) to fit within ginkgo framework"
+                    (let* ((match "if \\([a-zA-Z0-9.,']+\\) != \\([a-zA-Z-0-9.,']+\\) *{\n *fmt.Println(\"[a-z A-Z]*\")\n[\t| ]+return\n *}")
+                           (g-pre "framework.ExpectEqual\("))
+                      (replace-regexp-in-string match
+                                                (lambda (match)
+                                                  (save-match-data
+                                                    (let* ((comparisons (s-split " *!= *"(s-chop-prefix "if "(s-trim(car(s-slice-at "{" match))))))
+                                                           (c1 (car comparisons))
+                                                           (c2 (cadr comparisons))
+                                                           (err (cadr(s-split "[()]" match)))
+                                                           (g-err (s-concat g-pre c1 ", " c2 ", " err "\)")))
+                                                      g-err)))
+                                                mock-test))))
+            (eval .
+                  (defun apisnoop/format-comp-errors (mock-test)
+                    "Formats numeric comparison errors (podA >= podB etc) to fit within ginkgo framework"
+                    (let* ((match " *if \\([a-zA-Z0-9.]+\\) *\\(==\\|>=\\|<=\\|>\\|<\\) *[0-9]+ *{\n[ \ta-zA-Z()\".,'!\n]*}")
+                           (g-pre "       framework.Expect\("))
+                      (replace-regexp-in-string match
+                                                (lambda (match)
+                                                  (save-match-data
+                                                    (let* ((comparison (s-chop-prefix "if "(s-trim(car(s-slice-at "{" match)))))
+                                                           (err (cadr(s-split "[()]" match)))
+                                                           (g-err (s-concat g-pre comparison ", " err "\)")))
+                                                      g-err)))
+                                                mock-test))))
+            (eval .
                   (defun apisnoop/mock->ginkgo-test ()
                     "Takes the contents of the code block named 'Mock Test in Go' and runs it through a set of conversions so its body closer matches the ginkgo test framework.  It inputs this new code block beneath the heading named 'Ginkgo Test'.
 
@@ -64,11 +95,14 @@ This function assumes you have the appropriately named code block and heading, w
                     (interactive)
                     (save-excursion
                       (let* ((mock-test (apisnoop/get-mock-test-value)) 
-                             (ginkgo-test (apisnoop/format-nil-errors mock-test))
-                             (src-code-block (concat "#+NAME: Ginkgo Test\n"
+                             (ginkgo-test (-> mock-test
+                                              apisnoop/format-nil-errors
+                                              apisnoop/format-eq-errors
+                                              apisnoop/format-comp-errors))
+                             (src-code-block (concat "#+NAME: Ginkgo TTest\n"
                                                      "#+begin_src go\n"
-                                                     ginkgo-test
-                                                     "#+end_src\n")))
+                                                     (s-trim-right ginkgo-test)
+                                                     "\n#+end_src\n")))
                         (goto-char (org-find-entry-with-id "gt001"))
                         (goto-char (org-element-property :contents-begin (org-element-at-point)))
                         (let ((first-element (org-element-at-point)))
