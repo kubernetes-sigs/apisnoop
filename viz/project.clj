a7bfa29877e70ef0c4588f0e1f6bb3322561851b
Zach <zach@mitt.fritz.box>
2020-06-16 17:01:44 +1200

    add visulualization tool for report exports

--- viz/project.clj
+++ viz/project.clj
@@ -1,10 +1,11 @@
 (defproject viz "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [metasoarous/oz "1.6.0-alpha6"]]
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [metasoarous/oz "1.6.0-alpha6"]
+                 [cheshire "5.10.0"]]
   :main ^:skip-aot viz.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
