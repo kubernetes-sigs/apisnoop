87ec38e081583855c0c011e4f3b0dd336777f6b5
workZach <workzach@mitt.fritz.box>
2020-06-29 21:19:08 +1200

    add release switching draft to sunburst pages

--- src/components/sunburst/Prologue.svelte
+++ src/components/sunburst/Prologue.svelte
@@ -5,6 +5,7 @@
  import { release } from '../../store';
 
  const SPYGLASS_URL = 'https://prow.k8s.io/view/gcs/kubernetes-jenkins/logs'
+ let releaseSwitch = false;
 
  $: ({
    release: version,
@@ -19,6 +20,25 @@
 
 {#if release}
   <SectionHeader title='{version} In Depth'>
+    {#if releaseSwitch}
+      <ul class='releases'>
+        <li on:click={() => goto('1.19').then(l => location.reload())}>1.19</li>
+        <li on:click={() => goto('1.18').then(l => location.reload())}>1.18</li>
+        <li on:click={() => goto('1.17').then(l => location.reload())}>1.17</li>
+        <li on:click={() => goto('1.16').then(l => location.reload())}>1.16</li>
+        <li on:click={() => goto('1.15').then(l => location.reload())}>1.15</li>
+        </ul>
+        {:else}
     <em>Data from <a href="{link}" title="spyglass link" target="_blank_" rel="noreferrer noopener">an e2e test suite run</a>, from {date}</em>
+    {/if}
+    <button on:click={() => releaseSwitch = true}>switch release</button>
   </SectionHeader>
 {/if}
+
+
+<style>
+ ul.releases li {
+   display: inline;
+   margin-right: 1rem;
+   }
+</style>
