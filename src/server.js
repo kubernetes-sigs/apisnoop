2b004fdc0ff4e84685a0f92b829d34afec3f6ff0
workZach <workzach@mitt.fritz.box>
2020-06-29 21:18:57 +1200

    export app in server to work with vercel

--- src/server.js
+++ src/server.js
@@ -6,12 +6,14 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = polka() // You can also use Express
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
+
+export default app;
