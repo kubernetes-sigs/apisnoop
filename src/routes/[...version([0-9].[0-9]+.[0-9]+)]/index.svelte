c8ebf12d92f5a451937d3ee4e07f9842e257a8c6
workZach <workzach@mitt.fritz.box>
2020-07-02 14:56:57 +1200

    use full semver in client routes

---
+++ src/routes/[...version([0-9].[0-9]+.[0-9]+)]/index.svelte
@@ -0,0 +1,58 @@
+<script context="module">
+ export async function preload({params, query}) {
+   let [version] = params.version;
+   let level, category, endpoint;
+   let payload = {
+     version,
+     level,
+     category,
+     endpoint
+   };
+   return {payload};
+ }
+</script>
+
+<script>
+ import { afterUpdate } from 'svelte';
+ import { releasesURL } from '../../lib/constants.js';
+ import {
+   releases,
+   activeFilters,
+   activeRelease
+ } from '../../store';
+ import { isEmpty } from 'lodash-es';
+ import Sunburst from '../../components/sunburst/Wrapper.svelte'
+
+ export let payload;
+ $: ({
+   version,
+   level,
+   category,
+   endpoint
+ } = payload);
+
+ afterUpdate(async() => {
+   activeFilters.update(af => ({
+     ...af,
+     version,
+     level: level || '',
+     category: category || '',
+     endpoint: endpoint || ''
+   }))
+   if (isEmpty($activeRelease.endpoints)) {
+     let rel = await fetch(`${releasesURL}/${version}.json`)
+       .then(res => res.json());
+     releases.update(rels => ({...rels, [version]: rel}));
+   }
+ });
+</script>
+
+<svelte:head>
+  <title>APISnoop | {version}</title>
+</svelte:head>
+
+{#if $activeRelease && $activeRelease.endpoints.length > 0}
+<Sunburst />
+{:else}
+<em>Loading Data...</em>
+{/if}
