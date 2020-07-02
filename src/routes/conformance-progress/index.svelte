282ec0ec5691574dad7849463eedcfe70a452c78
workZach <workzach@mitt.fritz.box>
2020-07-02 19:49:06 +1200

    update conformance progress

--- src/routes/conformance-progress/index.svelte
+++ src/routes/conformance-progress/index.svelte
@@ -1,17 +1,22 @@
-<html lang='en-gb'>
+<html>
 <head>
     <meta charset="UTF-8" />
-    <title>Snoop Progress Report</title>
+    <title>Oz document</title>
     <meta content="Oz document" name="description" />
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <link href="http://ozviz.io/oz.svg" rel="shortcut icon" type="image/x-icon" />
-    <link href="main.css" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap" rel="stylesheet" />
+    <link href="http://ozviz.io/fonts/lmroman12-regular.woff" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/vega@5.9.0" type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/vega-lite@4.0.2" type="text/javascript"></script>
     <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.0.0" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML" type="text/javascript"></script>
+    <style>
+     body{font-size: 16px;}
+     canvas{margin-top: 1em; margin-bottom: 1em;}
+    </style>
 </head>
+
 <body>
     <main>
         <h1>APISnoop Progress reports</h1>
@@ -22,39 +27,39 @@
         </div>
         <h2>Stable Endpoint Coverage Over Time</h2><em>In this, we look at the total endpoints per release and how many came in with tests. We also track the overall testing coverage per release.</em>
         <div>
-            <div id="viz-17dd01b5-3a89-4a8a-9163-a49a83341d84"></div>
+            <div id="viz-505543ae-0f1b-4d4d-bbaa-72345cec126c"></div>
             <script type="text/javascript">
-                vegaEmbed('#viz-17dd01b5-3a89-4a8a-9163-a49a83341d84', {
+                vegaEmbed('#viz-505543ae-0f1b-4d4d-bbaa-72345cec126c', {
                     "data": {
                         "values": [{
                             "release": "1.9.0",
                             "type": "tested",
-                            "total": 53,
+                            "total": 61,
                             "order": "a"
                         }, {
                             "release": "1.9.0",
                             "type": "untested",
-                            "total": 211,
+                            "total": 203,
                             "order": "c"
                         }, {
                             "release": "1.9.0",
                             "type": "new_untested",
-                            "total": 49,
+                            "total": 42,
                             "order": "d"
                         }, {
                             "release": "1.9.0",
                             "type": "new_tested",
-                            "total": 14,
+                            "total": 21,
                             "order": "b"
                         }, {
                             "release": "1.10.0",
                             "type": "tested",
-                            "total": 67,
+                            "total": 82,
                             "order": "a"
                         }, {
                             "release": "1.10.0",
                             "type": "untested",
-                            "total": 260,
+                            "total": 245,
                             "order": "c"
                         }, {
                             "release": "1.10.0",
@@ -69,12 +74,12 @@
                         }, {
                             "release": "1.11.0",
                             "type": "tested",
-                            "total": 71,
+                            "total": 86,
                             "order": "a"
                         }, {
                             "release": "1.11.0",
                             "type": "untested",
-                            "total": 265,
+                            "total": 250,
                             "order": "c"
                         }, {
                             "release": "1.11.0",
@@ -89,12 +94,12 @@
                         }, {
                             "release": "1.12.0",
                             "type": "tested",
-                            "total": 71,
+                            "total": 86,
                             "order": "a"
                         }, {
                             "release": "1.12.0",
                             "type": "untested",
-                            "total": 267,
+                            "total": 252,
                             "order": "c"
                         }, {
                             "release": "1.12.0",
@@ -109,12 +114,12 @@
                         }, {
                             "release": "1.13.0",
                             "type": "tested",
-                            "total": 72,
+                            "total": 87,
                             "order": "a"
                         }, {
                             "release": "1.13.0",
                             "type": "untested",
-                            "total": 267,
+                            "total": 252,
                             "order": "c"
                         }, {
                             "release": "1.13.0",
@@ -129,12 +134,12 @@
                         }, {
                             "release": "1.14.0",
                             "type": "tested",
-                            "total": 72,
+                            "total": 87,
                             "order": "a"
                         }, {
                             "release": "1.14.0",
                             "type": "untested",
-                            "total": 267,
+                            "total": 252,
                             "order": "c"
                         }, {
                             "release": "1.14.0",
@@ -149,12 +154,12 @@
                         }, {
                             "release": "1.15.0",
                             "type": "tested",
-                            "total": 81,
+                            "total": 96,
                             "order": "a"
                         }, {
                             "release": "1.15.0",
                             "type": "untested",
-                            "total": 276,
+                            "total": 261,
                             "order": "c"
                         }, {
                             "release": "1.15.0",
@@ -169,12 +174,12 @@
                         }, {
                             "release": "1.16.0",
                             "type": "tested",
-                            "total": 94,
+                            "total": 107,
                             "order": "a"
                         }, {
                             "release": "1.16.0",
                             "type": "untested",
-                            "total": 263,
+                            "total": 250,
                             "order": "c"
                         }, {
                             "release": "1.16.0",
@@ -189,12 +194,12 @@
                         }, {
                             "release": "1.17.0",
                             "type": "tested",
-                            "total": 127,
+                            "total": 140,
                             "order": "a"
                         }, {
                             "release": "1.17.0",
                             "type": "untested",
-                            "total": 258,
+                            "total": 245,
                             "order": "c"
                         }, {
                             "release": "1.17.0",
@@ -209,12 +214,12 @@
                         }, {
                             "release": "1.18.0",
                             "type": "tested",
-                            "total": 136,
+                            "total": 150,
                             "order": "a"
                         }, {
                             "release": "1.18.0",
                             "type": "untested",
-                            "total": 250,
+                            "total": 236,
                             "order": "c"
                         }, {
                             "release": "1.18.0",
@@ -229,22 +234,22 @@
                         }, {
                             "release": "1.19.0",
                             "type": "tested",
-                            "total": 145,
+                            "total": 163,
                             "order": "a"
                         }, {
                             "release": "1.19.0",
                             "type": "untested",
-                            "total": 241,
+                            "total": 223,
                             "order": "c"
                         }, {
                             "release": "1.19.0",
                             "type": "new_untested",
-                            "total": 0,
+                            "total": 1,
                             "order": "d"
                         }, {
                             "release": "1.19.0",
                             "type": "new_tested",
-                            "total": 14,
+                            "total": 31,
                             "order": "b"
                         }]
                     },
@@ -259,7 +264,7 @@
                         "y": {
                             "field": "total",
                             "type": "quantitative",
-                            "title": "Total Stable! Endpoints"
+                            "title": "Total Stable Endpoints"
                         },
                         "color": {
                             "field": "type",
@@ -291,11 +296,11 @@
         <p>In 1.16, the community set a requirement for any endpoint promoted to come with a conformance test. That release was a standout in terms of coverage: a significant number of new endpoints were added, with the majority promoted with tests</p>
         <p>Our latest release is also looking extremely positive, with every new endpoint promoted comiong in with a test, along with the general testing coverage going up.</p>
         <h2>New endpoints per release and their current coverage</h2><em>In this chart we look at how many endpoints were promoted per release, and of that group, how many are untested today.</em>
-        <p>We start the report at 1.8, so this will have the most new endpoints (since it really starts with all the existing endpoints at that time). This report highlights the importance of having a gate that ensures any promoted endpoint comes with a test. This became an initiative in 1.16 and that release has one of the best ratios of introduced to tested. 1.19 is also looking impressive, with currently 100% coverage on all its endpoints.</p>
+        <p>We start the report at 1.9, so this will have the most new endpoints by default.This report highlights the importance of having a gate that ensures any promoted endpoint comes with a test. This became an initiative in 1.16 and that release has one of the best ratios of introduced to tested. 1.19 is also looking impressive, with nearly 100% coverage on all its new endpoints.</p>
         <div>
-            <div id="viz-372f7248-c723-44a9-b591-a8a8080e88fb"></div>
+            <div id="viz-798fdb6f-5bed-4083-9581-5e9d10487039"></div>
             <script type="text/javascript">
-                vegaEmbed('#viz-372f7248-c723-44a9-b591-a8a8080e88fb', {
+                vegaEmbed('#viz-798fdb6f-5bed-4083-9581-5e9d10487039', {
                     "data": {
                         "values": [{
                             "release": "1.9.0",
@@ -304,7 +309,7 @@
                         }, {
                             "release": "1.9.0",
                             "type": "still_untested",
-                            "total": 46
+                            "total": 39
                         }, {
                             "release": "1.10.0",
                             "type": "tested",
@@ -344,7 +349,7 @@
                         }, {
                             "release": "1.14.0",
                             "type": "still_untested",
-                            "total": 9
+                            "total": 7
                         }, {
                             "release": "1.15.0",
                             "type": "tested",
@@ -380,11 +385,11 @@
                         }, {
                             "release": "1.19.0",
                             "type": "tested",
-                            "total": 14
+                            "total": 32
                         }, {
                             "release": "1.19.0",
                             "type": "still_untested",
-                            "total": 0
+                            "total": 1
                         }]
                     },
                     "width": "900",
@@ -427,5 +432,6 @@
     </main>
     <div class="vg-tooltip" id="vis-tooltip"></div>
     <script src="js/compiled/oz.js" type="text/javascript"></script>
-    </body>
+</body>
+
 </html>
