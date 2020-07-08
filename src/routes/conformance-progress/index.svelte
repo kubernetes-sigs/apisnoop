b7f43a565e4b75204ee00784203af283d1c96675
Zach! <webmaster@coolguy.website>
2020-07-08 19:18:07 +1200

    track conformance progress as percentage chart

--- src/routes/conformance-progress/index.svelte
+++ src/routes/conformance-progress/index.svelte
@@ -3,7 +3,8 @@
    conformanceProgressRaw,
    formattedProgress,
    coveragePerReleaseRaw,
-   coveragePerRelease
+   coveragePerRelease,
+   conformanceProgressPercentage
  } from '../../store';
 
  import Link from '../../components/icons/link-solid.svelte';
@@ -12,12 +13,14 @@
  import { onMount } from 'svelte';
  import StableOverTime from '../../components/vega-charts/stable-over-time.svelte';
  import CoveragePerRelease from '../../components/vega-charts/coverage-per-release.svelte';
+ import ConformanceProgressPercentage  from '../../components/vega-charts/conformance-progress-percentage.svelte'
 
  onMount(async() => {
    let progressData = await fetch(`${releasesURL}/conformance-progress.json`).then(res => res.json());
    let coverageData = await fetch(`${releasesURL}/conformance-coverage-per-release.json`).then(res=>res.json());
    conformanceProgressRaw.set(progressData);
    coveragePerReleaseRaw.set(coverageData);
+   console.log({percent: $conformanceProgressPercentage});
  });
  // something
 </script>
@@ -45,6 +48,17 @@
 {/if}
 </section>
 
+<section id="conformance-progress-percentage">
+  <h2><a href="conformance-progress#conformance-progress-percentage">Stable Endpoint Coverage at Time Of Release(Percentage)<Link width="1.25rem" /> </a></h2>
+  <em>The percentage of the total endpoints hit by conformance tests at the time of release.</em>
+  {#if $conformanceProgressPercentage.length === 0}
+    <p>loading chart...</p>
+  {:else}
+    <ConformanceProgressPercentage data={$conformanceProgressPercentage} />
+  {/if}
+
+</section>
+
 <section id="coverage-by-release">
   <h2><a href="conformance-progress#coverage-by-release">Conformance Coverage By Release <Link width="1.25rem"/></a></h2>
 <em>In this chart we look at the promoted endpoints in a release, and how many of them are tested or untested today.</em>
@@ -55,6 +69,7 @@
 {:else}
   <CoveragePerRelease data={$coveragePerRelease} />
 {/if}
+
 
 <p>This report highlights the importance of having a gate that ensures any promoted endpoint comes with a test. This became an initiative in 1.16 and that release has one of the best ratios of introduced to tested. 1.19 is also looking impressive, with nearly 100% coverage on all its new endpoints.</p>
 </section>
@@ -71,3 +86,4 @@
    background: aliceblue;
  }
 </style>
+
