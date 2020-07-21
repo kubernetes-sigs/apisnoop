afd2bdbd55c5439f23e8a566e0f74c7083495c35
Zach! <webmaster@coolguy.website>
2020-07-21 13:25:40 +1200

    1.19 canonical latest release

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,5 @@
 export const releasesURL = 'https://raw.githubusercontent.com/apisnoop/snoopDB/master/resources/coverage';
 export const RELEASES = [
-  '1.20.0',
   '1.19.0',
   '1.18.0',
   '1.17.0',
