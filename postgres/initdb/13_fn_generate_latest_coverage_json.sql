0871463e38420f1bff9a611aeca54ce8a517a07c
zach mandeville <zz@ii.coop>
2020-07-22 03:16:30 +0000

    update sourced to sources in coverage json

--- postgres/initdb/13_fn_generate_latest_coverage_json.sql
+++ postgres/initdb/13_fn_generate_latest_coverage_json.sql
@@ -6,6 +6,7 @@ select release into latest_release from audit_event order by release::semver lim
 return(
   select row_to_json(c) from (
     select release, release_date, spec,
+           select array_agg(source) as sources,
            (select source from (select source from audit_event where release = latest_release limit 1) s) as source,
            (select array_agg(row_to_json(endpoint_coverage)) from endpoint_coverage where release = latest_release) as endpoints,
            (select array_agg(row_to_json(audit_event_test)) from audit_event_test where release = latest_release) as tests
