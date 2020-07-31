d117509d8ec65eb9bd5ff1a83aa746b2df00e9c8
zachmandeville <zz@ii.coop>
2020-07-31 13:42:52 +1200

    sort conformance endpoint table by any of its headers

--- src/routes/conformance-progress/endpoints/[release].svelte
+++ src/routes/conformance-progress/endpoints/[release].svelte
@@ -15,19 +15,16 @@
    confFilters,
    confFilteredEndpoints
  } from '../../../store/conformance.js';
+ import ConformanceEndpointsTable from '../../../components/conformance-endpoints-table.svelte';
 
  export let payload;
  const endpointsURL = 'https://raw.githubusercontent.com/apisnoop/snoopDB/master/resources/coverage/conformance-endpoints.json'
 
  let { release } = payload;
- $: filters = Object.keys($confFilters).filter(key => key !== 'release');
-
  onMount(async() => {
    const endpoints = await fetch(endpointsURL).then(res => res.json())
    confEndpointsRaw.set(endpoints);
  })
-
-
  afterUpdate(() => {
    if (release && $confFilters.release !== release) {
      confFilters.update(f => ({...f, release}));
@@ -37,45 +34,4 @@
 </script>
 <h2>Conformance Endpoints for {release} </h2>
 <p>{$confFilteredEndpoints.length}</p>
-<table>
-  <thead>
-    <tr>
-      <th>Endpoint</th>
-      <th>Promotion Release</th>
-      <th>Tested Release</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each $confFilteredEndpoints as {endpoint, colour, promotion_release, tested_release}}
-      <tr>
-        <td style="border-left: 2px solid {colour};">{endpoint}</td>
-        <td class="release">{promotion_release}</td>
-        <td class="release">
-          {#if tested_release}
-            {tested_release}
-            {:else}
-            not tested
-          {/if}
-        </td>
-      </tr>
-    {/each}
-  </tbody>
-</table>
-<style>
- thead th {
-   padding: 0.25rem;
-   border-right: 1px solid gray;
-   border-bottom: 1px solid gray;
- }
- tr {
-   margin: 0;
- }
- td {
-   padding: 0.25rem;
-   margin-bottom: none;
-   margin-right: 0.5rem;
- }
- td.release {
-   text-align: right;
- }
-</style>
+<ConformanceEndpointsTable endpoints="{$confFilteredEndpoints}" />
