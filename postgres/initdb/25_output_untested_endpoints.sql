f999dd07c753901a240d1151b0ddd382481a3e00
zach mandeville <zz@ii.coop>
2020-09-02 16:41:41 +1200

    add untested endpoint output script

---
+++ postgres/initdb/25_output_untested_endpoints.sql
@@ -0,0 +1,21 @@
+begin;
+\t
+\a
+\o '/tmp/untested-endpoints.txt'
+
+  with latest_release as (
+  select release::semver as release
+    from open_api
+   order by release::semver desc
+   limit 1
+  )
+
+  select endpoint
+    from conformance.new_endpoint ne
+           join latest_release on ne.release::semver = latest_release.release
+where tested is false;
+
+\o
+\a
+\t
+commit;
