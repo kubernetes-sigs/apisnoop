6303b6170aa867343e0affc60f0c19a4fc9438a3
zach mandeville <zz@ii.coop>
2020-10-16 11:33:38 +1300

    delete misleading snoopdb docs
    this is a remnant from merging the snoopdb repo into apisnoop, and had
    docs that did not fully develop.  Removing them to make our docs folder
    cleaner and more ready to blossom into full documentation.

--- docs/snoopdb/_firn/layouts/default.clj
+++
@@ -1,9 +0,0 @@
-(defn default
-  [{:keys [render partials site-url]}]
-  (let [{:keys [head]} partials]
-    (head site-url
-     [:body
-      [:main
-       [:article.content
-        ;; [:div (render :toc)] ;; Optional; add a table of contents
-        [:div (render :file)]]]])))
--- docs/snoopdb/_firn/partials/head.clj
+++
@@ -1,9 +0,0 @@
-(defn head
-  [site-url body]
-  (let [resource #(str site-url "/static/" %)]
-    [:html
-     [:head
-      [:meta {:charset "UTF-8"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-      [:link {:rel "stylesheet" :href (resource "css/firn_base.css")}]]
-     body]))
