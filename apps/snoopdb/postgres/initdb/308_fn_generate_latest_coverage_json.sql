d24da5b959a2eaeb07c59eeadd5afdd80bdf517d
zach mandeville <zz@ii.coop>
2020-10-29 13:43:28 +1300

    pretty print the outputjson for our coverage
    Co-authored-by: Berno Kleinhans <<bb@ii.coop>>

--- apps/snoopdb/postgres/initdb/308_fn_generate_latest_coverage_json.sql
+++ apps/snoopdb/postgres/initdb/308_fn_generate_latest_coverage_json.sql
@@ -1,10 +1,14 @@
+comment on function generate_latest_coverage_json is 'helper to create properly formatted json to be output as a  coverage/X.XX.json file';
+
+select 'generate_latest_coverage_json function defined and commented' as "build log";
+
 create function generate_latest_coverage_json()
 returns json as $$
 declare latest_release varchar;
 begin
 select release into latest_release from audit_event order by release::semver limit 1;
 return(
-select row_to_json(c) from (
+select jsonb_pretty(row_to_json(c)::jsonb) from (
     select release, release_date, spec,
         (select array_agg(source) from (select source from audit_event where release = latest_release group by source) s) as sources,
         (select array_agg(row_to_json(endpoint_coverage)) from endpoint_coverage where release = latest_release) as endpoints,
@@ -14,7 +18,3 @@ select row_to_json(c) from (
     group by release, release_date, spec) c);
 end;
 $$ language plpgsql;
-
-comment on function generate_latest_coverage_json is 'helper to create properly formatted json to be output as a  coverage/X.XX.json file';
-
-select 'generate_latest_coverage_json function defined and commented' as "build log";
