1bed920d18c2afc20f97fd258e9f2d783264c9c5
Zach Mandeville <zz@ii.coop>
2021-01-11 11:40:44 +1300

    load 1.20.0 in our historical releases

--- apps/snoopdb/postgres/initdb/500_load_all_open_api.sql
+++ apps/snoopdb/postgres/initdb/500_load_all_open_api.sql
@@ -1,27 +1,28 @@
-     begin;
-     with releases as (
-       select column1 as release
-         from (values
-         ('v1.5.0'),
-         ('v1.6.0'),
-         ('v1.7.0'),
-         ('v1.8.0'),
-         ('v1.9.0'),
-         ('v1.10.0'),
-         ('v1.11.0'),
-         ('v1.12.0'),
-         ('v1.13.0'),
-         ('v1.14.0'),
-         ('v1.15.0'),
-         ('v1.16.0'),
-         ('v1.17.0'),
-         ('v1.18.0'),
-         ('v1.19.0')
-         ) as rlist
-     )
-     select f.*
-       from
-       releases r
-       , lateral load_open_api(r.release) f("build log");
-     select * from load_open_api() f("build log");
-     commit;
+begin;
+with releases as (
+  select column1 as release
+    from (values
+    ('v1.5.0'),
+    ('v1.6.0'),
+    ('v1.7.0'),
+    ('v1.8.0'),
+    ('v1.9.0'),
+    ('v1.10.0'),
+    ('v1.11.0'),
+    ('v1.12.0'),
+    ('v1.13.0'),
+    ('v1.14.0'),
+    ('v1.15.0'),
+    ('v1.16.0'),
+    ('v1.17.0'),
+    ('v1.18.0'),
+    ('v1.19.0'),
+    ('v1.20.0')
+    ) as rlist
+)
+select f.*
+  from
+  releases r
+  , lateral load_open_api(r.release) f("build log");
+select * from load_open_api() f("build log");
+commit;
