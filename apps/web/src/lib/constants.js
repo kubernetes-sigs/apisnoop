086c89d002cffe2b7bc27e08d0299b729c56f2d1
Zach! <webmaster@coolguy.website>
2021-03-08 12:44:49 +1300

    remove logging

--- apps/web/src/lib/constants.js
+++ apps/web/src/lib/constants.js
@@ -1,12 +1,4 @@
-const branch = process.env.BRANCH;
-const commit = process.env.COMMIT;
 const head = process.env.HEAD;
-
-console.log({
-  branch: `https://raw.githubusercontent.com/cncf/apisnoop/${branch}/resources/coverage`,
-  commit: `https://raw.githubusercontent.com/cncf/apisnoop/${commit}/resources/coverage`,
-  head: `https://raw.githubusercontent.com/cncf/apisnoop/${head}/resources/coverage`
-});
 export const RELEASES_URL = head
   ? `https://raw.githubusercontent.com/cncf/apisnoop/${head}/resources/coverage`
   : 'https://raw.githubusercontent.com/cncf/apisnoop/master/resources/coverage';
