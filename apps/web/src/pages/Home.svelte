548e25961863e026762d3734a16d4114aeb41b9f
workzach <workzach@mbpii.fritz.box>
2021-04-21 18:10:30 +1200

    update releases with fetch on first visit of page

--- apps/web/src/pages/Home.svelte
+++ apps/web/src/pages/Home.svelte
@@ -1,63 +1,93 @@
 <script>
  import { onMount, afterUpdate } from 'svelte';
- import { RELEASES, RELEASES_URL } from '../lib/constants.js';
- import {isEmpty} from 'lodash-es';
+ import yaml from 'js-yaml';
+ import { gte } from 'semver';
  import {
-   activeFilters,
-   activeRelease,
-   latestVersion,
-   newEndpoints,
-   olderNewEndpointsRaw,
-   previousRelease,
-   releases
+     groupBy,
+     isEmpty,
+     mapValues,
+ } from 'lodash-es';
+ import { EARLIEST_VERSION, RELEASES_URL } from '../lib/constants.js';
+ import {
+     activeFilters,
+     activeRelease,
+     latestVersion,
+     newEndpoints,
+     olderNewEndpointsRaw,
+     previousRelease,
+     releases
  } from '../store';
  import Sunburst from '../components/Sunburst/Wrapper.svelte'
  import NewEndpoints from '../components/new-endpoints.svelte';
 
  export let params;
+ export let query;
 
  $: ({
-   version,
-   level,
-   category,
-   endpoint
-     } = params);
+     version,
+     level,
+     category,
+     endpoint
+ } = params);
 
  afterUpdate(async() => {
-   if (version === 'latest' || version == null) {
-    version = $latestVersion;
-   };
-   activeFilters.update(af => ({
-     ...af,
-     version,
-     level: level || '',
-     category: category || '',
-     endpoint: endpoint || ''
-   }))
-   if ($activeRelease !== 'older' && isEmpty($activeRelease.endpoints)) {
-     let rel = await fetch(`${RELEASES_URL}/${$activeRelease.release}.json`)
-       .then(res => res.json());
-     releases.update(rels => ({...rels, [$activeRelease.release]: rel}));
-   }
-   if ($previousRelease !== 'older' && !isEmpty($previousRelease) && isEmpty($previousRelease.endpoints)) {
-     let rel = await fetch(`${RELEASES_URL}/${$previousRelease.release}.json`)
-       .then(res => res.json());
-     releases.update(rels => ({...rels, [$previousRelease.release]: rel}));
-   }
-   if ($olderNewEndpointsRaw.length === 0) {
-     let older = await fetch(`${RELEASES_URL}/new-endpoints.json`)
-     .then(res=>res.json());
-     olderNewEndpointsRaw.set(older);
-   }
+     if ($releases && isEmpty($releases)) {
+         let releasesData = await
+         fetch(`${RELEASES_URL}/releases.yaml`)
+             .then(res => res.blob())
+             .then(blob => blob.text())
+             .then(yamlString => yaml.load(yamlString))
+             .then(releases => releases.filter(({version}) => gte(version, EARLIEST_VERSION)))
+             .then(releases => {
+                 return mapValues(groupBy(releases, 'version'),
+                                  ([{version, release_date}]) => ({
+                                      release: version,
+                                      release_date: release_date == '' ? new Date() : release_date,
+                                      spec: '',
+                                      source: '',
+                                      endpoints: [],
+                                      tests: []
+                 }))
+             });
+         releases.update(rel => releasesData);
+     }
+     if (version === 'latest' || version == null) {
+         version = $latestVersion;
+     };
+     activeFilters.update(af => ({
+         ...af,
+         version,
+         level: level || '',
+         category: category || '',
+         endpoint: endpoint || ''
+     }))
+
+     if (!isEmpty($activeRelease) && $activeRelease !== 'older' && isEmpty($activeRelease.endpoints)) {
+         let rel = await fetch(`${RELEASES_URL}/${$activeRelease.release}.json`)
+             .then(res => res.json());
+         releases.update(rels => ({...rels, [$activeRelease.release]: rel}));
+     }
+
+     if ($previousRelease !== 'older' && !isEmpty($previousRelease) && isEmpty($previousRelease.endpoints)) {
+         let rel = await fetch(`${RELEASES_URL}/${$previousRelease.release}.json`)
+             .then(res => res.json());
+         // todo why is this being called at certaint imes?
+         // releases.update(rels => ({...rels, [$previousRelease.release]: rel}));
+     }
+     if ($olderNewEndpointsRaw.length === 0) {
+         let older = await fetch(`${RELEASES_URL}/new-endpoints.json`)
+             .then(res=>res.json());
+         olderNewEndpointsRaw.set(older);
+     }
  });
-  </script>
+</script>
 
 <svelte:head>
-    <title>APISnoop | {$activeRelease.release}</title>
+    <title>APISnoop</title>
 </svelte:head>
-  {#if $activeRelease && $activeRelease.endpoints.length > 0}
+{#if $activeRelease && $activeRelease.endpoints && $activeRelease.endpoints.length > 0}
     <Sunburst />
     <NewEndpoints />
-  {:else}
-  <em>loading data...</em>
-  {/if}
+{:else}
+    <em>loading data...</em>
+{/if}
