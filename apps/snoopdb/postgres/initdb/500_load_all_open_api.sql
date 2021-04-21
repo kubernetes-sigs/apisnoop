f986c1c921708b913e2ec9dd23547ae0a759ba21
workzach <workzach@mbpii.fritz.box>
2021-04-21 12:40:39 +1200

    update load_all_open_api
    this now generates a dynamic list of past releases from our releases.yaml

--- apps/snoopdb/postgres/initdb/500_load_all_open_api.sql
+++ apps/snoopdb/postgres/initdb/500_load_all_open_api.sql
@@ -1,28 +1,7 @@
 begin;
-with releases as (
-  select column1 as release
-    from (values
-    ('v1.5.0'),
-    ('v1.6.0'),
-    ('v1.7.0'),
-    ('v1.8.0'),
-    ('v1.9.0'),
-    ('v1.10.0'),
-    ('v1.11.0'),
-    ('v1.12.0'),
-    ('v1.13.0'),
-    ('v1.14.0'),
-    ('v1.15.0'),
-    ('v1.16.0'),
-    ('v1.17.0'),
-    ('v1.18.0'),
-    ('v1.19.0'),
-    ('v1.20.0')
-    ) as rlist
-)
 select f.*
-  from
-  releases r
-  , lateral load_open_api(r.release) f("build log");
+    from
+    (select release from grab_past_releases() as release) r
+    , lateral load_open_api(r.release::text) f("build log");
 select * from load_open_api() f("build log");
 commit;
