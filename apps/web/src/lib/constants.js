90b90b1325e05ff1be1deb92ebfb2cf1696bbbdb
zim <webmaster@coolguy.website>
2023-02-17 16:17:45 +1300

    Add url for ineligible and pending endpoints
    Signed-off-by: zim <webmaster@coolguy.website>

--- apps/web/src/lib/constants.js
+++ apps/web/src/lib/constants.js
@@ -3,4 +3,6 @@ export const RELEASES_URL = head
   ? `https://raw.githubusercontent.com/cncf/apisnoop/${head}/resources/coverage`
   : 'https://raw.githubusercontent.com/cncf/apisnoop/main/resources/coverage';
 // the earliest release we have test data for
+export const PENDING_ENDPOINTS_URL = "https://raw.githubusercontent.com/ii/kubernetes/pending_eligible_endpoints/test/conformance/testdata/pending_eligible_endpoints.yaml";
+export const INELIGIBLE_ENDPOINTS_URL = "https://raw.githubusercontent.com/ii/kubernetes/pending_eligible_endpoints/test/conformance/testdata/ineligible_endpoints.yaml";
 export const EARLIEST_VERSION ='1.15.0';
