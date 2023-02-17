e0c076971ad69a1fa6fea89eefabc09957fe20f2
zim <webmaster@coolguy.website>
2023-02-17 16:21:04 +1300

    add toggles for pending and ineligible endpoints
    Signed-off-by: zim <webmaster@coolguy.website>

--- apps/web/src/components/Sunburst/ConformanceToggle.svelte
+++ apps/web/src/components/Sunburst/ConformanceToggle.svelte
@@ -3,33 +3,80 @@
 
  const conformanceLink = 'https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/conformance-tests.md#conformance-test-requirements';
 
- const isToggleInQuery = () =>
-     window.location.search.toLowerCase().includes("conformance-only=true");
+ function isFilterInQuery (filter)  {
+   const params = (new URL(document.location)).searchParams;
+   return params.has(filter);
+ }
 
  function toggleConformanceFilter ({target}) {
-     const path = window.location.pathname;
-     if (target.checked) {
-         page(`${path}?conformance-only=true`);
+   const path = window.location.pathname;
+   if (target.checked) {
+     page(`${path}?conformance-only=true`);
+   } else {
+     page(path);
+   }
+ }
+
+ function toggleFilter ({ target }) {
+   const params = (new URL(document.location)).searchParams;
+   const path = window.location.pathname;
+   if (target.checked) {
+     params.set(target.name,true)
+     page(`${path}?${params.toString()}`)
+   } else {
+     params.delete(target.name)
+     let paramString = params.toString();
+     if (paramString === "") {
+       page(`${path}`)
      } else {
-         page(path);
+       page(`${path}?${paramString}`)
      }
+   }
  }
 
 </script>
 
-<div>
+<form>
+  <div class='form-group'>
     <input type='checkbox'
-           checked={isToggleInQuery()}
-                 id='conformance-only-toggle'
-           name='conformance-only'
-           on:click={toggleConformanceFilter}
-    />
-    <label for='conformance-only'>Only show
-        <a href={conformanceLink}
-           target="_blank"
-           rel='nofollower noreferrer'>
-            conformance eligible endpoints
-        </a>
+           id='exclude-ineligible-toggle'
+           name='exclude-ineligible'
+           checked={isFilterInQuery('exclude-ineligible')}
+           on:click={toggleFilter} />
+    <label for='exclude-ineligible'>
+      exclude conformance ineligible endpoints
+      (<em><a href='/conformance-progress/ineligible-endpoints'>which ones are removed?</a></em>)
     </label>
-        <p>(<em><a href='/conformance-progress/ineligible-endpoints'>which ones are removed?</a></em>)</p>
-</div>
+  </div>
+  <div class='form-group'>
+    <input type='checkbox'
+           id='exclude-pending-tgogle'
+           name='exclude-pending'
+           checked={isFilterInQuery('exclude-pending')}
+           on:click={toggleFilter} />
+    <label for='exclude-pending'>
+      exclude endpoints whose eligibility is pending
+      <!-- (<em><a href='/conformance-progress/ineligible-endpoints'>which ones are removed?</a></em>) -->
+    </label>
+  </div>
+  <div class='form-group'>
+    <input type='checkbox'
+           id='conformance-only-toggle'
+           name='conformance-only'
+           checked={isFilterInQuery('conformance-only')}
+           on:click={toggleFilter} />
+    <label for='conformance-only'>Only show
+      <a href={conformanceLink}
+              target="_blank"
+         rel='nofollower noreferrer'>
+        conformance eligible endpoints
+      </a>
+    </label>
+  </div>
+</form>
+
+<style>
+ form {
+   margin-top: 1rem;
+ }
+</style>
