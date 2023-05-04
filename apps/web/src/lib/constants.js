423b5338f2b503049a54e0267e7035279643b7eb
zim <webmaster@coolguy.website>
2023-05-05 09:09:39 +1200

    update constants to k/k repo

--- apps/web/src/lib/constants.js
+++ apps/web/src/lib/constants.js
@@ -4,5 +4,5 @@ export const RELEASES_URL = head
   : 'https://raw.githubusercontent.com/cncf/apisnoop/main/resources/coverage';
 // the earliest release we have test data for
 export const PENDING_ENDPOINTS_URL = "https://raw.githubusercontent.com/kubernetes/kubernetes/master/test/conformance/testdata/pending_eligible_endpoints.yaml";
-export const INELIGIBLE_ENDPOINTS_URL = "https://raw.githubusercontent.com/ii/kubernetes/pending_eligible_endpoints/test/conformance/testdata/ineligible_endpoints.yaml";
+export const INELIGIBLE_ENDPOINTS_URL = "https://raw.githubusercontent.com/kubernetes/kubernetes/master/test/conformance/testdata/ineligible_endpoints.yaml";
 export const EARLIEST_VERSION ='1.15.0';
