2c169eadaf6924299a01de8b1fd1fbd951323646
zachmandeville <webmaster@coolguy.website>
2023-06-16 14:39:27 +1200

    wip add work in cluster
    Signed-off-by: zachmandeville <webmaster@coolguy.website>

--- apps/snoopdb/postgres/initdb/308_fn_determine_endpoint.sql
+++ apps/snoopdb/postgres/initdb/308_fn_determine_endpoint.sql
@@ -1,9 +1,14 @@
 create or replace function determine_endpoint() RETURNS TRIGGER as $$
+   import os
    import json
    from snoopUtils import load_openapi_spec, find_operation_id
-   CURRENT_SWAGGER_URL = "https://raw.githubusercontent.com/kubernetes/kubernetes/master/api/openapi-spec/swagger.json"
+
+   MASTER_SWAGGER_URL = "https://raw.githubusercontent.com/kubernetes/kubernetes/master/api/openapi-spec/swagger.json"
+   incluster = os.getenv('KUBERNETES_PORT')
+   open_api_url = "cluster" if incluster else MASTER_SWAGGER_URL
+
    if "spec" not in GD:
-       GD["spec"] = load_openapi_spec(CURRENT_SWAGGER_URL)
+       GD["spec"] = load_openapi_spec(url)
    spec = GD["spec"]
    event = json.loads(TD["new"]["data"])
    if TD["new"]["endpoint"] is None:
