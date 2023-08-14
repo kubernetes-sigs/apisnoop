7615ffb49042d5332afbefde1f361355b34b67ee
Hippie Hacker <hh@ii.coop>
2023-08-14 08:19:17 +0100

    Add two blank lines between functions

--- apps/snoopdb/postgres/snoopUtils.py
+++ apps/snoopdb/postgres/snoopUtils.py
@@ -120,7 +120,6 @@ def cluster_swagger():
     auth = {"Authorization": "Bearer " + token}
     return requests.get(url, headers=auth, verify=False).json()
 
-
 def load_openapi_spec(url):
     """
     Load given swagger url into a cache, so we can use it later to find operation id's
